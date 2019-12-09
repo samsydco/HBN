updates to g diff code - correcting mds calculation, and how g_diff is calculated in time

6_ISC_sh.py
@@ -14,7 +14,9 @@ from ISC_settings import *
 del phenol['all']
 phenolperm = phenol
 
-ISCf = ISCpath+'ISC_'+str(date.today())+'_sh_2.h5'
+start_date = str(date.today())
+
+ISCf = ISCpath+'ISC_'+start_date+'_sh_2.h5'
 if os.path.exists(ISCf):
     os.remove(ISCf)
 dd.io.save(ISCf,{'subs':subord,'ages':agel,'phenodict':phenol,'pcs':pcl})
@@ -71,6 +73,7 @@ for s in range(nsh):
 			grpt = hf.create_group(task+str(s))
 			good_v_indexes = {key: np.arange(81924) for key in phenol.keys()}
 			for shuff in tqdm.tqdm(range(nshuff+1)):
+				print(start_date)
 				grps = grpt.require_group('shuff_'+str(shuff))
 				for k,v in phenol.items():
 					grpk = grps.require_group(k)

7_bin_g_diff.py
@@ -64,8 +64,10 @@ for f in ROIl:
 		df = df[cols[-2:]+cols[:-2]]
 		df = df.reindex(cols[-2:]+cols[:-2])
 		dfs[roin][task] = df
-		embedding = MDS(n_components=1, dissimilarity='precomputed')
-		X_transformed[roin][task] = embedding.fit_transform(np.nan_to_num(df.values))
+		X_transformed[roin][task] = {}
+		for ndims in [1,2]:
+			embedding = MDS(n_components=ndims, dissimilarity='precomputed')
+			X_transformed[roin][task][str(ndims)] = embedding.fit_transform((1-np.nan_to_num(df.values)))
 for task in ['DM']:#dfs[roin].keys():
 	allvals = [dfs[r][task].values for r in dfs.keys()]
 	for roin in dfs.keys():
@@ -80,20 +82,29 @@ for task in ['DM']:#dfs[roin].keys():
 		plt.tight_layout()
 		plt.show()
 		ax.figure.savefig(figurepath+'SfN_2019/Figure_3_thresh/'+task+'_'+roin+'.png')
+
 # display mds
 xlab = df.columns.tolist()
-figs = {key: plt.figure().add_subplot(111) for key in ['DM','TP']}
-for fi,v in enumerate(X_transformed):
-	for task,vv in X_transformed[v].items():
-		ax = figs[task]
-		ax.set_yticks([],[])
-		ax.set_xticks(vv,[])
-		#ax[task][v].set_xticklabels([])
-		ax.plot(vv,np.zeros(len(vv)),'.',label=' '.join(v[3:].split('_')))
-for task in figs.keys():
-	figs[task].set_xticklabels(xlab)
-	lgd = figs[task].legend(loc='lower right')
-	figs[task].figure.savefig(figurepath+'SfN_2019/Figure_3/'+task+'_MDS.png')
-		
+plt.rcParams.update({'font.size': 8})
+for task in ['DM','TP']:
+	fig, ax = plt.subplots(nrows=5, ncols=2, figsize = (4,10))
+	for i,roiv in enumerate(X_transformed.items()):
+		for ndims,data in roiv[1][task].items():
+			axid = int(ndims)-1
+			xvals = data if ndims == '1' else data[:,0]
+			yvals = np.zeros(len(xlab)) if ndims == '1' else data[:,1]
+			ax[i,axid].scatter(xvals,yvals)
+			ax[i,axid].set_xlim(-.2,.2)
+			ax[i,axid].set_ylim(-.2,.2)
+			#if ndims == 1: ax[i,axid].set_ylim(-.5,.5)
+			ax[i,axid].set_aspect(1.0)
+			for ii, txt in enumerate(xlab):
+				sub = 0.05 if ndims =='2' else 0
+				rot = 0 if ndims =='2' else 30
+				ax[i,axid].annotate(txt, xy=(xvals[ii],yvals[ii]+sub),ha='center',rotation=rot)
+			ax[i,axid].set_title('ROI:'+roiv[0])
+	plt.tight_layout()
+	plt.show()
+	fig.savefig(figurepath+'agediff_g_diff/'+task+'_MDS.png')
 		
 	

7_g_diff_time.py
@@ -15,46 +15,77 @@ from settings import *
 from ISC_settings import *
 
 gdiffroidir = path+'ROIs/SfN_2019/Fig3_'#'ROIs/g_diff/'
-g_diff_f = ISCpath+'g_diff_time_'+str(date.today())+'.h5'
-if os.path.exists(g_diff_f):
-    os.remove(g_diff_f)
 nTR=750
 ROIl = glob.glob(gdiffroidir+'*roi')
 nROI = len(ROIl)
-gdict = {}
-for f in tqdm.tqdm(ROIl):
-	fn = f.split(gdiffroidir)[1]
-	roin = fn[:-7]
-	task = 'DM' if fn[:2] == 'TP' else 'TP'
-	hemi = fn[3]
-	roi = np.loadtxt(f).astype('int')
-	vall = roi[roi[:,1]==1,0]
-	n_vox = len(vall)
-	groups = np.zeros((2,n_vox,nTR),dtype='float16')
-	for i,b in enumerate([0,nbinseq-1]):
-		subl = [ageeq[i][1][b][idx] for i in [0,1] for idx in np.random.choice(lenageeq[i][b],minageeq[i],replace=False)]
-		# Load data and z-score within a group:	
-		group = np.zeros((n_vox,nTR),dtype='float16')
-		groupn = np.ones((n_vox,nTR),dtype='int')*len(subl)
-		for sub in subl:
-			d = dd.io.load(sub,['/'+task+'/'+hemi],sel=dd.aslice[vall,:])[0]
-			group = np.nansum(np.stack((group,d)),axis=0)
-			nanverts = np.argwhere(np.isnan(d))
-			groupn[nanverts[:, 0],nanverts[:,1]] = groupn[nanverts[:,0],nanverts[:,1]]-1
-		groups[i] = zscore(group/groupn,axis=0)
-	gdict[roin] = np.sum(np.multiply(groups[0],groups[1]),axis=0)/(n_vox-1)
-dd.io.save(g_diff_f,gdict)
+ses = [0,10,20,30,40,50,60]
+
+def calc_isc(g1,g2,n_vox):
+	ISCt = np.sum(np.multiply(g1,g2),axis=0)/(n_vox-1)
+	return ISCt
+
+#g_diff_f = ISCpath+'g_diff_time_2019-10-04.h5'
+g_diff_f = ISCpath+'g_diff_time_'+str(date.today())+'.h5'
+if g_diff_f[-13:-3] == str(date.today()):
+	if os.path.exists(g_diff_f):
+		os.remove(g_diff_f)
+	gdict = {}
+	for f in tqdm.tqdm(ROIl):
+		fn = f.split(gdiffroidir)[1]
+		roin = fn[:-7]
+		task = 'DM' if fn[:2] == 'TP' else 'TP'
+		hemi = fn[3]
+		roi = np.loadtxt(f).astype('int')
+		vall = roi[roi[:,1]==1,0]
+		n_vox = len(vall)
+		groups = np.zeros((2,2,n_vox,nTR),dtype='float16')
+		for i,b in enumerate([0,nbinseq-1]):
+			subl = [[],[]]
+			for ii in [0,1]:
+				subg = [ageeq[ii][1][b][idx] for idx in np.random.choice(lenageeq[ii][b],divmod(minageeq[ii],2)[0]*2,replace=False)]
+				subl[0].extend(subg[:divmod(minageeq[i],2)[0]])
+				subl[1].extend(subg[divmod(minageeq[i],2)[0]:])
+			for h in [0,1]:
+				# Load data and z-score within a group:	
+				group = np.zeros((n_vox,nTR),dtype='float16')
+				groupn = np.ones((n_vox,nTR),dtype='int')*len(subl)
+				for sub in subl[h]:
+					d = dd.io.load(sub,['/'+task+'/'+hemi],sel=dd.aslice[vall,:])[0]
+					group = np.nansum(np.stack((group,d)),axis=0)
+					nanverts = np.argwhere(np.isnan(d))
+					groupn[nanverts[:, 0],nanverts[:,1]] = groupn[nanverts[:,0],nanverts[:,1]]-1
+				groups[i,h] = zscore(group/groupn,axis=0)
+		ISCg = np.zeros(nTR) 
+		for htmp1 in [0,1]:
+			for htmp2 in [0,1]:
+				ISCg += calc_isc(groups[0,htmp1],groups[1,htmp2],n_vox)
+		ISC0 = calc_isc(groups[0,0],groups[0,1],n_vox)
+		ISC1 = calc_isc(groups[1,0],groups[1,1],n_vox)
+		ISCg[ISCg<0] = 0
+		ISC0[ISC0<0] = 0
+		ISC1[ISC1<0] = 0
+		gdict[roin] = {}
+		for s in ses:
+			ham = np.hamming(np.round(s/0.8))
+			ham = ham/np.sum(ham)
+			if s==0:
+				gdict[roin][str(s)] = ISCg/4/(np.sqrt(ISC0)*np.sqrt(ISC1))
+				gdict[roin][str(s)][np.isfinite(gdict[roin][str(s)])] = 0
+			else:
+				gdict[roin][str(s)] = np.convolve(ISCg,ham,'same')/4/(
+					np.sqrt(np.convolve(ISC0,ham,'same'))*np.sqrt(np.convolve(ISC1,ham,'same')))
+	dd.io.save(g_diff_f,gdict)
+else:
+	gdict = dd.io.load(g_diff_f)
 xticks = np.arange(0,600,0.8) #x axis in seconds
-for s in [0,10,20,30]:
+for s in ses:
 	plt.figure()
-	ham = np.hamming(np.round(s/0.8))
-	ham = ham/np.sum(ham)
 	plt.style.use('seaborn-muted')
 	for r,t in gdict.items():
 		if ham.size==0:
-			plt.plot(xticks,t,label=r[6:])
+			plt.plot(xticks,t[str(s)],label=r[6:])
 		else:
-			plt.plot(xticks,np.convolve(t,ham,'same'),label=r[6:])
+			plt.plot(xticks,np.convolve(t[str(s)],ham,'same'),label=r[6:])
 		plt.xlabel('time (sec)')
 		plt.ylabel('ISC (r)')
 	plt.legend()