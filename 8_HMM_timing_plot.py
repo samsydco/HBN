82bad64a7873d69cd1148cf9b4215961ee46edc0
Samantha Cohen <samantha.s.cohen@gmail.com>
2020-02-28 17:06:10 -0500

    Changed 8_HMM_timing_plot code - saved more things to output file (log likelihood fits for best fitting k's) for plotting on brain.

--- 8_HMM_timing_plot.py
+++ 8_HMM_timing_plot.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 from HMM_settings import *
 
 ROIopts = ['YeoROIsforSRM_sel_2020-01-14.h5','YeoROIsforSRM_2020-01-03.h5','SfN_2019/ROIs_Fig3/Fig3_','g_diff/']
-ROInow = ROIopts[0]
+ROInow = ROIopts[1]
 HMMf = HMMpath+'timing_'+ROInow+'/'
 ROIs = glob.glob(HMMf+'*h5')
 
@@ -22,7 +22,7 @@ for roi in tqdm.tqdm(ROIs):
 	for ti,task in enumerate(tasks):
 		yvsospred[task][roi_short] = {key: {} for key in ['best_tune_ll','best_corr']}
 		time = np.arange(TR,nTR[ti]*TR+1,TR)[:-1]
-		x_list = list(np.arange(TR, nTR[ti]/TR, TR))
+		yvsospred[task][roi_short]['best_tune_ll']['ll'] = np.max(np.mean(ROIsHMM[task]['tune_ll'],axis=0))
 		yvsospred[task][roi_short]['best_tune_ll']['k'] = k_list[ROIsHMM[task]['best_tune_ll']]
 		yvsospred[task][roi_short]['best_corr']['k'] = k_list[ROIsHMM[task]['best_corr']]
 		D = [np.mean(ROIsHMM[task]['bin_0']['D'],axis=0).T,
@@ -33,8 +33,8 @@ for roi in tqdm.tqdm(ROIs):
 			hmm.fit(D)
 			fig, ax = plt.subplots(figsize=(10, 10))
 			ax.set_title(roi_short+' '+kstr+' '+task, fontsize=50)
-			ax.set_xticks(time[0::50])
-			ax.set_xticklabels([str(int(s//60))+':'+str(int(s%60)) for s in time][0::50], fontsize=30)
+			ax.set_xticks(time[0::nTR[ti]//5])
+			ax.set_xticklabels([str(int(s//60))+':'+str(int(s%60)) for s in time][0::nTR[ti]//5], fontsize=30)
 			ax.set_xlabel('Time (seconds)', fontsize=35)
 			ax.set_yticks(kl)
 			ax.set_yticklabels(kl,fontsize=30)
@@ -50,7 +50,7 @@ for roi in tqdm.tqdm(ROIs):
 			ax.text(time[-1], 2, 'Avg prediction = ',verticalalignment='bottom', horizontalalignment='right', fontsize=35)
 			yvsospred[task][roi_short][kstr]['auc_diff'] = round((auc[1]-auc[0])/(k['k'])*TR, 2)
 			ax.text(time[-1]-10, 1, str(round((auc[1]-auc[0])/(k['k'])*TR, 2)) + ' seconds', verticalalignment='bottom', horizontalalignment='right', fontsize=35)
-			plt.show()
+			#plt.show()
 			plt.savefig(figurepath+'HMM/timing/'+roi_short+'_'+kstr+'_'+task+'.png', bbox_inches='tight')
 
 dd.io.save(HMMpath+'f_timing_'+ROInow,yvsospred)
