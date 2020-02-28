2a7720a2fb6e201f12b2431f334717a2467e6617
Samantha Cohen <samantha.s.cohen@gmail.com>
2020-02-28 17:53:02 -0500

    I think I made it s.t. 3_run_singularity actually works, fingers crossed.

--- 3_run_singularity.py
+++ 3_run_singularity.py
@@ -14,22 +14,23 @@ path_tmp = path if site =='Site-RU' else path+site+'/'
 
 for chunk in tqdm(pchunk):
 	pstr = ' '.join(chunk)
-	for task in ['DM','TP']:
+	for task in ['DM']:#,'TP']:
 		date = str(datetime.datetime.now())[0:19].replace(' ','_')
 		f = open("%sfmriprep_cmdoutput/%s_%s_%s.txt"%(path,task,date,pstr.replace(" ","_")), "w")
 		command = ('singularity run --cleanenv \
-		/data/Loci/fmriprep-1.5.6.simg \
 		-B '+path_tmp+':/data \
 		-B '+outputdr+':/out \
-		-B '+tmpdr+':/work \
+		-B '+tmpdr+'/:/work \
+		/data/Loci/fmriprep-1.5.6.simg \
 		/data /out \
 		participant \
 		--ignore=slicetiming \
 		--use-syn-sdc \
 		--fs-license-file=/data/fs_license.txt \
 		--output-spaces '+output_space+' \
-		-w /scratch \
+		-w /work/ \
 		--participant-label '+pstr+' -t movie'+task).split()
-		sp.call(command,\
+		p = sp.Popen(command,\
 				 stdin=sp.PIPE, stderr=sp.PIPE,\
 				 universal_newlines=True,stdout=f)
+		p.communicate('/n')[1]
