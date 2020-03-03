c80abf9eb51799cf3d722581509563864d4eb8ab
Samantha Cohen <samantha.s.cohen@gmail.com>
2020-03-03 14:38:08 -0500

    updated 8_HMM_timing_plot.py s.t. area under log likelihood curve was saved for both young and old groups when compared to ll on r.h.s. of curves.

--- 8_HMM_timing_plot.py
+++ 8_HMM_timing_plot.py
@@ -8,6 +8,7 @@ import numpy as np
 import deepdish as dd
 import brainiak.eventseg.event
 import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
 from HMM_settings import *
 
 ROIopts = ['YeoROIsforSRM_sel_2020-01-14.h5','YeoROIsforSRM_2020-01-03.h5','SfN_2019/ROIs_Fig3/Fig3_','g_diff/']
@@ -20,8 +21,9 @@ for roi in tqdm.tqdm(ROIs):
 	roi_short = roi.split('/')[-1][:-3]
 	ROIsHMM = dd.io.load(roi)
 	for ti,task in enumerate(tasks):
+		nTR_ = nTR[ti]
 		yvsospred[task][roi_short] = {key: {} for key in ['best_tune_ll','best_corr']}
-		time = np.arange(TR,nTR[ti]*TR+1,TR)[:-1]
+		time = np.arange(TR,nTR_*TR+1,TR)[:-1]
 		yvsospred[task][roi_short]['best_tune_ll']['ll'] = np.max(np.mean(ROIsHMM[task]['tune_ll'],axis=0))
 		yvsospred[task][roi_short]['best_tune_ll']['k'] = k_list[ROIsHMM[task]['best_tune_ll']]
 		yvsospred[task][roi_short]['best_corr']['k'] = k_list[ROIsHMM[task]['best_corr']]
@@ -33,8 +35,8 @@ for roi in tqdm.tqdm(ROIs):
 			hmm.fit(D)
 			fig, ax = plt.subplots(figsize=(10, 10))
 			ax.set_title(roi_short+' '+kstr+' '+task, fontsize=50)
-			ax.set_xticks(time[0::nTR[ti]//5])
-			ax.set_xticklabels([str(int(s//60))+':'+str(int(s%60)) for s in time][0::nTR[ti]//5], fontsize=30)
+			ax.set_xticks(time[0::nTR_//5])
+			ax.set_xticklabels([str(int(s//60))+':'+str(int(s%60)) for s in time][0::nTR_//5], fontsize=30)
 			ax.set_xlabel('Time (seconds)', fontsize=35)
 			ax.set_yticks(kl)
 			ax.set_yticklabels(kl,fontsize=30)
@@ -52,6 +54,101 @@ for roi in tqdm.tqdm(ROIs):
 			ax.text(time[-1]-10, 1, str(round((auc[1]-auc[0])/(k['k'])*TR, 2)) + ' seconds', verticalalignment='bottom', horizontalalignment='right', fontsize=35)
 			#plt.show()
 			plt.savefig(figurepath+'HMM/timing/'+roi_short+'_'+kstr+'_'+task+'.png', bbox_inches='tight')
+
+def extend_for_TP(array,task):
+	xnans = np.nan*np.zeros(4)
+	if task == 'DM':
+		array = array
+	else:
+		array = np.concatenate((array,xnans), axis=0)
+	return array
+	
+# Now plotting difference in tune_ll between young and old:
+for roi in tqdm.tqdm(ROIs):
+	roi_short = roi.split('/')[-1][:-3]
+	ROIsHMM = dd.io.load(roi)
+	fig,ax = plt.subplots(2,sharex=True)
+	axa = fig.add_subplot(111, frameon=False)
+	axa.set_title(roi_short)
+	axa.set_xlabel('Average Event Duration',labelpad=20)
+	axa.set_ylabel('Log likelihood\nPer TR',labelpad=40)
+	axa.set_yticks([],[])
+	axa.set_xticks([],[])
+	for ti,task in enumerate(tasks):
+		nTR_ = nTR[ti]
+		x_list = [np.round(TR*(nTR_/k),2) for k in k_list]
+		if task == 'TP': x_list = x_list+[120,150,200,300]; ax[ti].set_xticks(x_list,[]); ax[ti].set_xticklabels([])
+		ax[ti].set_title(task)
+		for b in bins:
+			yvsospred[task][roi_short]['bin_'+str(b)] = {}
+			# save both ll diff using r.h.s. baseline, and ll diff at max-ll k
+			yvsospred[task][roi_short]['bin_'+str(b)]['tune_ll'] = np.mean(ROIsHMM[task]['bin_'+str(b)]['tune_ll'],0)/nTR_
+			yvsospred[task][roi_short]['bin_'+str(b)]['max_ll_ll'] = yvsospred[task][roi_short]['bin_'+str(b)]['tune_ll'] \
+												[np.where(k_list==yvsospred[task][roi_short]['best_tune_ll']['k'])[0][0]]
+			c = '#1f77b4' if b == 0 else '#ff7f0e'
+			lab = 'Ages '+str(int(round(eqbins[b])))+' - '+str(int(round(eqbins[b+1])))
+			y = extend_for_TP(np.mean(ROIsHMM[task]['bin_'+str(b)]['tune_ll'],0)/nTR_,task)
+			yerr = extend_for_TP(np.std(ROIsHMM[task]['bin_'+str(b)]['tune_ll'],0)/nTR_,task)
+			ax[ti].errorbar(x_list, y, yerr=yerr, color=c, label=lab)	
+	ax[ti].set_xticklabels(x_list,rotation=45)
+	lgd = ax[ti].legend(loc='lower right', bbox_to_anchor=(1.3, 0))
+	fig.set_size_inches(9,6)
+	fig.tight_layout()
+	#plt.show()
+	fig.savefig(figurepath+'HMM/tune_ll_timing/'+roi_short+'.png', bbox_extra_artists=(lgd,), bbox_inches='tight')
+	
+
+# check that r.h.s. of ll's are aproximately equal across tune_ll, and each bin's tune_ll:
+# It looks like avg. of 2 bin's r.h.s. ll's will be approximately equal
+ROIshortl = list(wtf[task].keys())
+tune_ll = {key: {key: {} for key in ROIshortl} for key in tasks}
+bin_ll = {key: {key: {key: {} for key in ROIshortl} for key in tasks} for key in ['bin_0','bin_4']}
+for roi in tqdm.tqdm(ROIs):
+	roi_short = roi.split('/')[-1][:-3]
+	ROIsHMM = dd.io.load(roi)
+	for ti,task in enumerate(tasks):
+		nTR_ = nTR[ti]
+		tune_ll[task][roi_short] = np.mean(ROIsHMM[task]['tune_ll'],axis=0)[-1]/nTR_
+		for b in bins:
+			bin_ll['bin_'+str(b)][task][roi_short] = np.mean(ROIsHMM[task]['bin_'+str(b)]['tune_ll'],0)[-1]/nTR_
+
+fig,ax = plt.subplots(2,sharex=True)
+axa = fig.add_subplot(111, frameon=False)
+axa.set_xlabel('ROI label',labelpad=20)
+axa.set_ylabel('Log likelihood\nPer TR',labelpad=40)
+axa.set_yticks([],[])
+axa.set_xticks([],[])
+for ti,task in enumerate(tasks):
+	nTR_ = nTR[ti]
+	ys = {}
+	for b in bins:
+		c = '#1f77b4' if b == 0 else '#ff7f0e'
+		lab = 'Ages '+str(int(round(eqbins[b])))+' - '+str(int(round(eqbins[b+1])))
+		ys['bin_'+str(b)] = []
+		for v in bin_ll['bin_'+str(b)][task].values():
+			ys['bin_'+str(b)].append(v)
+		ax[ti].plot(ys['bin_'+str(b)], color=c, label=lab)
+	for roi in tqdm.tqdm(ROIs):
+		roi_short = roi.split('/')[-1][:-3]
+		ROIsHMM = dd.io.load(roi)
+		rhs = np.mean([bin_ll['bin_0'][task][roi_short],bin_ll['bin_4'][task][roi_short]])
+		for b in bins:
+			yvsospred[task][roi_short]['bin_'+str(b)]['ll>rhs'] = np.sum((np.mean(ROIsHMM[task]['tune_ll'],axis=0)/nTR_)[(np.mean(ROIsHMM[task]['tune_ll'],axis=0)/nTR_)>rhs])
+	ax[ti].set_title(task+', r = '+str(np.round(np.corrcoef(ys['bin_0'],ys['bin_4'])[0,1],2)))
+	ys['all'] = []
+	for v in tune_ll[task].values():
+		ys['all'].append(v)
+	ax[ti].plot(ys['all'], color='#008000', label='Avg both age groups')
+ax[ti].set_xticklabels(ROIshortl,rotation=45)
+lgd = ax[ti].legend(loc='lower right', bbox_to_anchor=(1.3, 0))
+fig.set_size_inches(9,6)
+fig.tight_layout()
+#plt.show()
+fig.savefig(figurepath+'HMM/tune_ll_timing/RHS_ll.png', bbox_extra_artists=(lgd,), bbox_inches='tight')
+		
+	
+
+			
 
 dd.io.save(HMMpath+'f_timing_'+ROInow,yvsospred)
 			
