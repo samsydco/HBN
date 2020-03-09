c7d0175f0383f72bc5eef19db384544239175e0e
Samantha Cohen <samantha.s.cohen@gmail.com>
2020-03-09 18:31:00 -0400

    Made changes to 6_ISC_sh.py: (1) consistent subjects used across shuffles. Saved that subject list. (2) Tried to fix bug in shuff_check wherein number of voxels is reduced based on significance.

--- 6_ISC_sh.py
+++ 6_ISC_sh.py
@@ -27,86 +27,94 @@ if smallsub == True:
 	agel,pcl,phenol = make_phenol(subord)
 	ISCfs = ISCpath+'ISC_small/ISC_small_'+subord[-1].split('/')[-1][4:-3]+'_'
 	
-
 del phenol['all']
 phenolperm = phenol
+conds = ['age','sex'] #phenol.keys() (only analyzing age and sex!)
 
 nsh = 1 #5 split-half iterations
 
-def ISCe_calc(fstr,cond):
-	ISCe = dd.io.load(fstr,'/'+cond+'/'+'ISC_w')[0]-dd.io.load(fstr,'/'+cond+'/'+'ISC_w')[1]
+def ISCe_calc(fstr):
+	ISCe = dd.io.load(fstr,'/'+'ISC_w')[0]-dd.io.load(fstr,'/'+'ISC_w')[1]
 	return ISCe
-def ISCg_calc(fstr,cond):
-    ISCg = sum(dd.io.load(fstr,'/'+cond+'/'+'ISC_b'))/4/(np.sqrt(dd.io.load(fstr,'/'+cond+'/'+'ISC_w')[0])*
-				   np.sqrt(dd.io.load(fstr,'/'+cond+'/'+'ISC_w')[1]))
+def ISCg_calc(fstr):
+    ISCg = sum(dd.io.load(fstr,'/'+'ISC_b'))/4/(np.sqrt(dd.io.load(fstr,'/'+'ISC_w')[0])*
+				   np.sqrt(dd.io.load(fstr,'/'+'ISC_w')[1]))
     return ISCg
 
-def shuff_check(fstr,cond,nshuff,n_vox):
+def shuff_check(fstr,nshuff,good_v_indexes):
 	fstrtmp = ('_').join(fstr.split('_')[:-1])+'_'+str(0)+'.h5'
-	ISCg = ISCg_calc(fstrtmp,cond)
-	ISCe = ISCe_calc(fstrtmp,cond)
+	ISCg = [v for i,v in ISCg_calc(fstrtmp) if i in good_v_indexes]
+	ISCe = [v for i,v in ISCe_calc(fstrtmp) if i in good_v_indexes]
 	vvecte = np.zeros((nshuff,len(ISCe)))
 	vvectg = np.zeros((nshuff,len(ISCg)))
-	goodvtmp = dd.io.load(('_').join(fstr.split('_')[:-1])+'_'+str(nshuff)+'.h5','/'+cond+'/'+'good_v_indexes')
 	for shuff in np.arange(1,nshuff+1):
 		fstrtmp = ('_').join(fstr.split('_')[:-1])+'_'+str(shuff)+'.h5'
-		vvecte[shuff-1,goodvtmp] = ISCe_calc(fstrtmp,cond)
-		vvectg[shuff-1,goodvtmp] = ISCg_calc(fstrtmp,cond)
+		vvecte[shuff-1] = ISCe_calc(fstrtmp)
+		vvectg[shuff-1] = ISCg_calc(fstrtmp)
 	vertsg = np.asarray([np.sum(vvectg[:,v]<ISCg[v])/nshuff for v in range(len(ISCg))]) #(previously: np.sum(vvect<ISC[v]))
 	vertse = np.asarray([np.sum(abs(vvecte[:,v])>abs(ISCe[v]))/nshuff for v in range(len(ISCe))])
-	vertsidxe = [i for i, v in enumerate(vertse) if v<0.1 and ~np.isnan(ISCe[i]) and                            vvecte[-1,i]!=0]
-	vertsidxg = [i for i, v in enumerate(vertsg) if v<0.1 and ~np.isnan(ISCg[i]) and                            vvectg[-1,i]!=0]
+	vertsidxe = [good_v_indexes[i] for i in [i for i, v in enumerate(vertse) \
+				 if v<0.1 and ~np.isnan(ISCe[i]) and vvecte[-1,i]!=0]]
+	vertsidxg = [good_v_indexes[i] for i in [i for i, v in enumerate(vertsg) \
+				 if v<0.1 and ~np.isnan(ISCg[i]) and vvectg[-1,i]!=0]]
 	good_v_indexes = list(set(vertsidxe+vertsidxg))
 	return good_v_indexes
 
-
-nshuff = 1#0000 # number of shuffles
+nshuff = 100#0000 # number of shuffles
 for s in range(nsh):
+	phenol_split = {key:{key:[] for key in conds} for key in conds}
+	subs = {key:[] for key in conds}
+	for k in conds:
+		v2 = phenolperm['sex'] if k!='sex' else phenolperm['age']
+		subh = even_out(phenolperm[k],v2)
+		subs_idx = [item2 for sublist in subh for item in sublist for item2 in item] # keeping subjects CONSISTENT across shuffles!
+		subs[k] = [sub for i,sub in enumerate(subord) if i in subs_idx]
+		for cond2 in conds:
+			phenol_split[k][cond2] = [p for i,p in enumerate(phenolperm[cond2]) if i in subs_idx]
 	for task in ['DM','TP']:
-		phenol = phenolperm
-		sh = dd.io.load(subord[0],['/'+task+'/L'])[0].shape
-		#D = np.empty((len(subord),sh[0]*2,sh[1]),dtype='float16') # UGH memory error :(
-		#for sidx, sub in tqdm.tqdm(enumerate(subord)):
-		#	D[sidx,:,:] = np.concatenate([dd.io.load(sub,['/'+task+'/L'])[0], dd.io.load(sub,['/'+task+'/R'])[0]], axis=0)
-		#D = np.transpose(D,(1,2,0))
-		n_vox=sh[0]*2
-		n_time = sh[1]
-		n_subj = len(subord)
-		#n_vox,n_time,n_subj=D.shape
-		good_v_indexes = {key: np.arange(n_vox) for key in phenol.keys()}
-		for shuff in tqdm.tqdm(range(nshuff+1)):
-			fstr = ISCfs+task+str(s)+'_shuff_'+str(shuff)+'.h5'
-			shuffdict = {k:{'subs':[],'ISC_w':[],'ISC_b':[],
-							'good_v_indexes':good_v_indexes[k]} for k in ['age','sex']} #phenol.keys()}
-			for k in shuffdict.keys():
-				n_vox = len(good_v_indexes[k])
-				v2 = phenolperm['sex'] if k!='sex' else phenolperm['age']
-				shuffdict[k]['subs'] = even_out(phenol[k],v2)
+		n_time = dd.io.load(subord[0],['/'+task+'/L'])[0].shape[1]
+		n_vox = 81924
+		for k in conds:
+			subord_k = subs[k]
+			phenol = phenol_split[k]
+			D = np.empty((len(subord_k),n_vox,n_time),dtype='float16') 
+			for sidx, sub in tqdm.tqdm(enumerate(subord_k)):
+				D[sidx,:,:] = np.concatenate([dd.io.load(sub,['/'+task+'/L'])[0], dd.io.load(sub,['/'+task+'/R'])[0]], axis=0)
+			D = np.transpose(D,(1,2,0))
+			good_v_indexes = np.arange(n_vox)
+			for shuff in tqdm.tqdm(range(nshuff+1)):
+				fstr = ISCfs+task+'_'+k+'_shuff_'+str(shuff)+'.h5'
+				shuffdict = {'subord':[],'subh':[],'ISC_w':[],'ISC_b':[],
+							'good_v_indexes':good_v_indexes}
+				good_v_indexes = shuffdict['good_v_indexes']
+				n_vox = len(good_v_indexes)
+				v2 = phenol_split[k]['sex'] if k!='sex' else phenol_split[k]['age']
+				shuffdict['subord'] = subord_k
+				shuffdict['subs'] = even_out(phenol[k],v2)
 				groups = np.zeros((2,2,n_vox,n_time),dtype='float16')
 				for h in [0,1]: # split all or between T / F
 					for htmp in [0,1]: # split within split
 						group = np.zeros((n_vox,n_time),dtype='float16')
-						groupn = np.ones((n_vox,n_time),dtype='int')*len(shuffdict[k]['subs'][h][htmp])
-						for i in shuffdict[k]['subs'][h][htmp]: # mem error in next line (91 reps):
-							D = np.concatenate([dd.io.load(subord[i],['/'+task+'/L'])[0], dd.io.load(subord[i],['/'+task+'/R'])[0]], axis=0)
-							group = np.nansum(np.stack((group,D[good_v_indexes[k],:])),axis=0)
-							nanverts = np.argwhere(np.isnan(D[good_v_indexes[k],:]))
+						groupn = np.ones((n_vox,n_time),dtype='int')*len(shuffdict['subs'][h][htmp])
+						for i in shuffdict['subs'][h][htmp]:
+							group = np.nansum(np.stack((group,\
+														D[good_v_indexes,:,i])),axis=0)
+							nanverts = np.argwhere(np.isnan(D[good_v_indexes,:,i]))
 							groupn[nanverts[:, 0],nanverts[:,1]] = groupn[nanverts[:,0],nanverts[:,1]]-1
 						groups[h,htmp] = zscore(group/groupn,axis=1)
-					shuffdict[k]['ISC_w'].append(np.sum(np.multiply(groups[h,0],groups[h,1]),axis=1)/(n_time-1))
+					shuffdict['ISC_w'].append(np.sum(np.multiply(groups[h,0],groups[h,1]),axis=1)/(n_time-1))
 				for htmp1 in [0,1]:
 					for htmp2 in [0,1]:
-						shuffdict[k]['ISC_b'].append(np.sum(np.multiply(groups[0,htmp1],groups[1,htmp2]),axis=1)/(n_time-1)) # correlate across groups
+						shuffdict['ISC_b'].append(np.sum(np.multiply(groups[0,htmp1],groups[1,htmp2]),axis=1)/(n_time-1)) # correlate across groups
 				if any(shu==shuff for shu in [101,1001]):
-					good_v_indexes[k] = shuff_check(fstr,k,shuff-1,n_vox)
+					shuffdict['good_v_indexes'] = shuff_check(fstr,shuff-1,good_v_indexes)
 					print('The number of verts left after',str(shuff),\
-						  'iterations for group',k,'is',len(good_v_indexes[k]))
-			dd.io.save(fstr,shuffdict)
-				
+						  'iterations for group',k,'is',len(good_v_indexes))
+			dd.io.save(fstr,shuffdict)	
 			# randomly shuffle phenol:
-			for k,v in phenol.items():
-				nonnanidx = np.argwhere(~np.isnan(phenol[k]))
+			for cond,v in phenol.items():
+				nonnanidx = np.argwhere(~np.isnan(phenol[cond]))
 				randidx = np.random.permutation(nonnanidx)
-				phenol[k] = [v[randidx[nonnanidx==idx][0]] if idx in nonnanidx else i for idx,i in enumerate(v)]
+				phenol[cond] = [v[randidx[nonnanidx==idx][0]] if idx in nonnanidx else i for idx,i in enumerate(v)]
 		
 		
