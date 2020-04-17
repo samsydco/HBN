9e49ba6e57b073a8fe538b362cb22515ef5da525
Samantha Cohen <samantha.s.cohen@gmail.com>
2020-04-17 17:24:03 -0400

    Editing 7_HMM_stats to incorporate new Yeo parcellation, editing both to correct for KFold error - made consistend across shuffled iterations (also edited 7_HMM_sl).

--- 7_HMM_sl.py
+++ 7_HMM_sl.py
@@ -15,7 +15,7 @@ SLlist = dd.io.load(ISCpath+'SLlist.h5')
 savedir = HMMpath+'SL/'
 nsub = 41
 nvox = 81924//2
-kf = KFold(n_splits=nsplit,shuffle=True)
+kf = KFold(n_splits=nsplit, shuffle=True, random_state=2)
 
 
 for ti,task in enumerate(tasks):
@@ -48,12 +48,11 @@ for ti,task in enumerate(tasks):
 			Dsplit = [Dsl[:nsub],Dsl[nsub:]] # split young and old
 			tune_ll = np.zeros((2,nsplit,len(k_list)))
 			tune_seg = {key:{key:np.zeros((nsplit,nTR_,key)) for key in k_list} for key in range(len(bins))}
-			for split in range(nsplit):
-				LI,LO = next(kf.split(np.arange(nsub)))
-				Dtrain = [np.mean(Dsplit[0][LI],axis=0).T,
-						  np.mean(Dsplit[1][LI],axis=0).T]
-				Dtest =  [np.mean(Dsplit[0][LO],axis=0).T,
-						  np.mean(Dsplit[1][LO],axis=0).T]
+			for split,Ls in enumerate(kf.split(np.arange(nsub),y)):
+				Dtrain = [np.mean(Dsplit[0][Ls[0]],axis=0).T,
+						  np.mean(Dsplit[1][Ls[0]],axis=0).T]
+				Dtest =  [np.mean(Dsplit[0][Ls[1]],axis=0).T,
+						  np.mean(Dsplit[1][Ls[1]],axis=0).T]
 				for ki,k in enumerate(k_list):
 					hmm = brainiak.eventseg.event.EventSegment(n_events=k)
 					hmm.fit(Dtrain)
@@ -69,7 +68,7 @@ for ti,task in enumerate(tasks):
 			SLdict['ll_diff'].append(ll_diff)
 			auc = []
 			for bi in range(len(bins)):
-				auc.append(np.dot(np.mean(tune_seg[bi][best_k],axis=0), np.arange(best_k)).sum())
+				auc.append(np.dot(np.mean(tune_seg[bi][best_k],axis=0), np.arange(best_k)+1).sum())
 			auc_diff = (auc[1]-auc[0])/(best_k)*TR
 			voxdict['auc_diff'][voxl] += auc_diff
 			SLdict['auc_diff'].append(auc_diff)
--- 7_HMM_stats.py
+++ 7_HMM_stats.py
@@ -35,8 +35,94 @@ ROInow = ROIopts[1]
 HMMf = HMMpath+'timing_'+ROInow+'/'
 savedir = HMMpath+'shuff/'
 ROIs = glob.glob(HMMf+'*h5')
-nsub = 41
-kf = KFold(n_splits=nsplit,shuffle=True)
+nsub= 41
+y = [0]*int(np.floor(nsub/nsplit))*4+[1]*(int(np.floor(nsub/nsplit))+1)
+kf = KFold(n_splits=nsplit, shuffle=True, random_state=2)
+
+import nibabel.freesurfer.io as free
+for hemi in glob.glob(path+'ROIs/annot/*')
+	print(hemi)
+	lab = free.read_annot(hemi)
+	for ri,roi_tmp in enumerate(lab[2]):
+		roi=roi_tmp.decode("utf-8")
+		roidict = {}
+		vall = np.where(lab[0]==ri)[0]
+		roidict['hemi'] = hemi.split('/')[-1][0].capitalize()
+		for ti,task in enumerate(tasks):
+			roidict[task] = {}
+			nTR_ = nTR[ti]
+			# Need to fit HMM for all k to find the best:
+			for b in bins:
+				roidict[task]['bin_'+str(b)] = {}
+				subl = [ageeq[i][1][b][idx] for i in [0,1] for idx in np.random.choice(lenageeq[i][b],minageeq[i],replace=False)]
+				roidict[task]['bin_'+str(b)]['subl'] = subl
+				nsub = len(subl)
+				# Load data
+				D = np.empty((nsub,len(vall),nTR_),dtype='float16')
+				badvox = []
+				for sidx, sub in enumerate(subl):
+					D[sidx,:,:] = dd.io.load(sub,['/'+task+'/'+roidict['hemi']],sel=dd.aslice[vall,:])[0]
+					badvox.extend(np.where(np.isnan(D[sidx,:,0]))[0]) # Some subjects missing some voxels
+				D = np.delete(D,badvox,1)
+				vall = np.delete(vall,badvox)
+				roidict['vall'] = vall
+				roidict[task]['bin_'+str(b)]['D'] = D
+			if len(vall) > 0:
+				tune_ll = np.zeros((2,nsplit,len(k_list)))
+				tune_seg = {key:{key:np.zeros((nsplit,nTR_,key)) for key in k_list} for key in range(len(bins))}
+				for split,Ls in enumerate(kf.split(np.arange(nsub),y)):
+					Dtrain = [np.mean(roidict[task]['bin_0']['D'][Ls[0]],axis=0).T,
+							  np.mean(roidict[task]['bin_4']['D'][Ls[0]],axis=0).T]
+					Dtest =  [np.mean(roidict[task]['bin_0']['D'][Ls[1]],axis=0).T,
+							  np.mean(roidict[task]['bin_4']['D'][Ls[1]],axis=0).T]
+					for ki,k in enumerate(k_list):
+						hmm = brainiak.eventseg.event.EventSegment(n_events=k)
+						hmm.fit(Dtrain)
+						for bi in range(len(bins)):
+							tune_seg[bi][k][split], tune_ll[bi,split,ki] = \
+										hmm.find_events(Dtest[bi])
+				roidict[task]['tune_ll'] = tune_ll
+				roidict[task]['tune_seg'] = tune_seg
+				#Now calculating best_k from average ll:
+				best_k = k_list[np.argmax(np.mean(np.mean(tune_ll,0),0))]
+				roidict[task]['best_k'] = best_k
+				tune_seg = np.zeros((nshuff+1,2,nsplit,nTR_,best_k))
+				tune_ll = np.zeros((nshuff+1,2,nsplit))
+				for split,Ls in enumerate(kf.split(np.arange(nsub),y)):
+					Dtrain = [np.mean(D[0][Ls[0]],axis=0).T,
+							  np.mean(D[1][Ls[0]],axis=0).T]
+					Dtest_all =  np.concatenate([D[0][Ls[1]],D[1][LO]])
+					nsubLO = len(Ls[1])
+					subl = np.arange(nsubLO*2) # subject list to be permuted!
+					hmm = brainiak.eventseg.event.EventSegment(n_events=best_k)
+					hmm.fit(Dtrain)
+					for shuff in range(nshuff+1):
+						Dtest = [np.mean(Dtest_all[subl[:nsubLO]],axis=0).T,
+								 np.mean(Dtest_all[subl[nsubLO:]],axis=0).T]
+						for bi in range(len(bins)):
+							tune_seg[shuff,bi,split], tune_ll[shuff,bi,split] = hmm.find_events(Dtest[bi])
+						# RANDOMIZE
+						subl = np.random.permutation(nsubLO*2)
+				roidict[task]['tune_ll'] = tune_ll
+				for shuff in range(nshuff+1):
+					shuffstr = 'shuff_'+str(shuff)
+					roidict[task][shuffstr] = {}
+					E_k = []
+					auc = []
+					for bi in range(len(bins)):
+						E_k.append(np.dot(np.mean(tune_seg[shuff,bi],axis=0), np.arange(best_k)+1))
+						auc.append(E_k[bi].sum())
+					roidict[task][shuffstr]['E_k'] = E_k
+					roidict[task][shuffstr]['E_k'] = auc
+					roidict[task][shuffstr]['auc_diff'] = ((auc[1]-auc[0])/best_k)*TR
+					roidict[task][shuffstr]['ll_diff'] = np.mean(tune_ll[shuff,1,:]) - np.mean(tune_ll[shuff,0,:])
+				
+			
+				
+        
+        
+
+
 
 for roi in tqdm.tqdm(ROIs):
 	roi_short = roi.split('/')[-1][:-3]
@@ -71,13 +157,18 @@ for roi in tqdm.tqdm(ROIs):
 						tune_seg[shuff,bi,split], tune_ll[shuff,bi,split] = hmm.find_events(Dtest[bi])
 					# RANDOMIZE
 					subl = np.random.permutation(nsubLO*2)
-			for shuff in range(nshuff+1):
+			roidict[task]['tune_ll'] = tune_ll
+            for shuff in range(nshuff+1):
 				shuffstr = 'shuff_'+str(shuff)
-				roidict[task][shuffstr] = {}		
+				roidict[task][shuffstr] = {}
+                E_k = []
 				auc = []
 				for bi in range(len(bins)):
-					auc.append(np.dot(np.mean(tune_seg[shuff,bi],axis=0), np.arange(best_k)).sum())
-				roidict[task][shuffstr]['auc_diff'] = (auc[1]-auc[0])/(best_k)*TR
+                    E_k.append(np.dot(np.mean(tune_seg[shuff,bi],axis=0), np.arange(best_k)+1))
+					auc.append(E_k[bi].sum())
+                roidict[task][shuffstr]['E_k'] = E_k
+                roidict[task][shuffstr]['E_k'] = auc
+				roidict[task][shuffstr]['auc_diff'] = ((auc[1]-auc[0])/best_k)*TR
 				roidict[task][shuffstr]['ll_diff'] = np.mean(tune_ll[shuff,1,:]) - np.mean(tune_ll[shuff,0,:])
 	dd.io.save(savedir+roi_short+'.h5',roidict)
 		
