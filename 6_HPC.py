35a320e6be8bed932c3011616dd315bf68f759fd
Samantha Cohen <samantha.s.cohen@gmail.com>
2020-06-23 16:40:41 -0400

    Added more p-values to  6_HPC.py.

--- 6_HPC.py
+++ 6_HPC.py
@@ -12,6 +12,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from ISC_settings import *
 from scipy.stats import zscore
+
+# Remove subjects over max(eqbins) age:
+incl_idx = [a<eqbins[-1] for a in agel]
+subord = [s for si,s in enumerate(subord) if incl_idx[si]]
+agel,pcl,phenol = make_phenol(subord)
 agelperm = agel
 phenolperm = phenol
 event_list = [56,206,244,343,373,404,443,506,544]
@@ -22,28 +27,34 @@ n_time=750
 nsub=40
 TW = 30
 TR=0.8
-nsh = 100 # number of split half iterations
+nsh = 1000 # number of split half iterations
 colors_age = ['#edf8fb','#b3cde3','#8c96c6','#8856a7','#810f7c']
 colors_ev = ['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999']
 x = np.arange(-1*TW//2,TW//2)*TR
 xstim = np.arange(n_time)*TR
 
 # ISC: within and between
-subla = {key:{key:{key:[] for key in range(nbinseq)} for key in range(nsh)} for key in [True,False]}
 D = {key:{} for key in range(nbinseq)}
-ISC_w_time = np.zeros((2,nsh,nbinseq,n_time))
-ISC_w = np.zeros((2,nsh,nbinseq))
-ISC_b_time = {{key:{} for key in range(nsh)} for key in [True,False]}
-ISC_b = {{key:{} for key in range(nsh)} for key in [True,False]}
-ISC_g_time = {{key:{} for key in range(nsh)} for key in [True,False]}
-gdict = {{key:{'Age1':[],'Age2':[],'g_diff':[]} for key in range(nsh)} for key in [True,False]}
+bootvs = ['bootstrap','shuffle','splithalf']
+boots = len(bootvs)
+subla = {key:{key:{key:[] for key in range(nbinseq)} for key in range(nsh)} for key in bootvs}
+ISC_w_time = np.zeros((boots,nsh,nbinseq,n_time))
+ISC_w = np.zeros((boots,nsh,nbinseq))
+ISC_b_time = {key:{key:{} for key in range(nsh)} for key in bootvs}
+ISC_b = {key:{key:{} for key in range(nsh)} for key in bootvs}
+ISC_g_time = {key:{key:{} for key in range(nsh)} for key in bootvs}
+gdict = {key:{key:{'Age1':[],'Age2':[],'g_diff':[]} for key in range(nsh)} for key in bootvs}
 
-for boot in [True,False]:
-	bootidx = 0 if True else 1
-	print('Bootstrapping') if boot == True else print('Not bootstrapping')
+for bootidx,bootv in enumerate(bootvs):
+	boot = True if bootv=='bootstrap' else False
+	print(bootv)
 	agel = agelperm
 	phenol = phenolperm
-	for s in tqdm.tqdm(range(nsh)): # Either permuted shuffle or bootstrapped resample iterations
+	for s in tqdm.tqdm(range(nsh)): # Either permuted shuffle,  bootstrapped resample iterations, or split-half iterations
+		if bootv == 'shuffle': # randomly shuffle ages:
+			ageidx = np.random.permutation(len(agel))
+			agel = [agel[ageidx[idx]] for idx,age in enumerate(agel)]
+			phenol['sex'] = [phenol['sex'][ageidx[idx]] for idx,age in enumerate(phenol['sex'])]
 		ageeq,lenageeq,minageeq = binagesubs(agel,phenol['sex'],eqbins,subord)
 		groups = np.zeros((nbinseq,2,n_time),dtype='float16')
 		for b in range(nbinseq):
@@ -53,62 +64,91 @@ for boot in [True,False]:
 				subl[0].extend(subg[:divmod(minageeq[i],2)[0]])
 				subl[1].extend(subg[divmod(minageeq[i],2)[0]:])
 			for sub in subl[0]+subl[1]:
-				if boot == True and sub not in D[b]:
+				if bootv != 'shuffle' and sub not in D[b]:
 					D[b][sub] = np.mean(zscore(dd.io.load(sub,['/'+task+'/HPC'])[0],axis=1),axis=0)
-				elif boot == False and all(sub not in D[b_] for b_ in range(nbinseq)):
-					realbin = np.sum([agelperm[[sub.split('/')[-1] for sub in subord].index(sub.split('/')[-1])] > e for e in eqbins]) - 1
+				elif bootv == 'shuffle' and all(sub not in D[b_] for b_ in range(nbinseq)):
+					realbin = np.sum([agelperm[[sub.split('/')[-1] for sub in subord].index(sub.split('/')[-1])] >= e for e in eqbins]) - 1
 					D[realbin][sub] = np.mean(zscore(dd.io.load(sub,['/'+task+'/HPC'])[0],axis=1),axis=0)
-			subla[boot][s][b].append(subl)
+			subla[bootv][s][b].append(subl)
 			for h in [0,1]: # split all or between T / F
 				group = np.zeros((n_time),dtype='float16')
 				groupn = np.ones((n_time),dtype='int')*nsub
 				for sub in subl[h]:
-					realbin = np.sum([agelperm[[sub.split('/')[-1] for sub in subord].index(sub.split('/')[-1])] > e for e in eqbins]) - 1
+					realbin = np.sum([agelperm[[sub.split('/')[-1] for sub in subord].index(sub.split('/')[-1])] >= e for e in eqbins]) - 1
 					group = np.nansum(np.stack((group,D[realbin][sub])),axis=0)
 					nanverts = np.argwhere(np.isnan(D[realbin][sub]))
 					groupn[nanverts] = groupn[nanverts]-1
 				groups[b,h] = zscore(group/groupn)
 			ISC_w_time[bootidx,s,b] = np.multiply(groups[b,0],groups[b,1])
-			ISC_w[bootidx,s,b] = np.sum(ISC_w_time[s,b])/(n_time-1)
+			ISC_w[bootidx,s,b] = np.sum(ISC_w_time[bootidx,s,b])/(n_time-1)
 		for p in itertools.combinations(range(nbinseq),2):
 			p_str = str(p[0])+'_'+str(p[1])
-			ISC_g_time[boot][s][p_str] = []
-			ISC_b_time[boot][s][p_str] = []
+			ISC_g_time[bootv][s][p_str] = []
+			ISC_b_time[bootv][s][p_str] = []
 			for htmp1 in [0,1]:
 				for htmp2 in [0,1]:
-					ISC_b_time[s][p_str].append(np.multiply(groups[p[0],htmp1], groups[p[1],htmp2]))
-			ISC_b[boot][s][p_str] = [np.sum(t)/(n_time-1) for t in ISC_b_time[s][p_str]] # correlate across bins
-			ISCg_time = np.sum(ISC_b_time[s][p_str],axis=0)	
-			ISCg = np.sum(ISC_b[s][p_str])	
-			ISC_g_time[boot][s][p_str] = ISCg_time/4/(np.sqrt(ISC_w[s,p[0]])*np.sqrt(ISC_w[s,p[1]]))
-			ISCg = ISCg/4/(np.sqrt(ISC_w[s,p[0]])*np.sqrt(ISC_w[s,p[1]]))
+					ISC_b_time[bootv][s][p_str].append(np.multiply(groups[p[0],htmp1], groups[p[1],htmp2]))
+			ISC_b[bootv][s][p_str] = [np.sum(t)/(n_time-1) for t in ISC_b_time[bootv][s][p_str]] # correlate across bins
+			ISCg_time = np.sum(ISC_b_time[bootv][s][p_str],axis=0)	
+			ISCg = np.sum(ISC_b[bootv][s][p_str])
+			denom = np.sqrt(ISC_w[bootidx,s,p[0]])*np.sqrt(ISC_w[bootidx,s,p[1]])
+			ISC_g_time[bootv][s][p_str] = ISCg_time/4/(denom)
+			ISCg = ISCg/4/(denom)
 			#if ISCg>1: ISCg=1
-			for k in gdict[s].keys():
+			for k in gdict[bootv][s].keys():
 				ir = [0,1] if '1' in k else [1,0]
 				if 'Age' in k:
 					for i in ir:
-						gdict[s][k].append(str(int(round(eqbins[p[i]])))+' - '+str(int(round(eqbins[p[i]+1])))+' y.o.')
-					gdict[boot][s]['g_diff'].extend([ISCg])
-		if boot == False: # randomly shuffle ages:
+						gdict[bootv][s][k].append(str(int(round(eqbins[p[i]])))+' - '+str(int(round(eqbins[p[i]+1])))+' y.o.')
+					gdict[bootv][s]['g_diff'].extend([ISCg])
+		if bootv == 'shuffle': # randomly shuffle ages:
 			ageidx = np.random.permutation(len(agel))
 			agel = [agel[ageidx[idx]] for idx,age in enumerate(agel)]
 			phenol['sex'] = [phenol['sex'][ageidx[idx]] for idx,age in enumerate(phenol['sex'])]
 			
-dd.io.save(ISCpath+'HPC.h5',{'D':D,'subla':subla, 'ISC_w_time':ISC_w_time, 'ISC_w':ISC_w, 'ISC_b_time':ISC_b_time, 'ISC_b':ISC_b, 'ISC_g_time':ISC_g_time, 'ISC_g_time_2':ISC_g_time_2, 'gdict':gdict})
+dd.io.save(ISCpath+'HPC.h5',{'D':D,'subla':subla, 'ISC_w_time':ISC_w_time, 'ISC_w':ISC_w, 'ISC_b_time':ISC_b_time, 'ISC_b':ISC_b, 'ISC_g_time':ISC_g_time, 'gdict':gdict})
+
+# Are within ISCs significantly greater than 0?
+p_within_greater_zero = []
+for b in range(nbinseq):
+	p_within_greater_zero.append(np.sum(ISC_w[0,:,b]<0)/nsh)
+# Is real difference between groups' ISC_w significant?
+wshuffdiff = []
+for p in itertools.combinations(range(nbinseq),2):
+	wshuffdiff.extend(ISC_w[1,:,p[0]]-ISC_w[1,:,p[1]])
+p_within_difference = {}
+for p in itertools.combinations(range(nbinseq),2):
+	p_str = str(p[0])+'_'+str(p[1])
+	p_within_difference[p_str] = np.sum(np.mean(ISC_w[2,:,p[0]]-ISC_w[2,:,p[1]]) < wshuffdiff)/len(wshuffdiff)
+# Are any g_diff ISCs significantly below shuffle?
+gshuff = []
+for s in range(nsh):
+	gshuff.extend(np.unique(gdict['shuffle'][s]['g_diff']))
+p_g = {}
+for p in itertools.combinations(range(nbinseq),2):
+	Ages = []
+	for a in [0,1]:
+		Ages.append(str(int(round(eqbins[p[a]])))+' - '+str(int(round(eqbins[p[a]+1])))+' y.o.')
+	idxs = []
+	for idx in ['Age1','Age2']:
+		idxs.append([ai for ai,a in enumerate(gdict['splithalf'][0][idx]) if any(aa==a for aa in Ages)])
+	idx = list(set(idxs[0]).intersection(set(idxs[1])))[0]
+	p_str = str(p[0])+'_'+str(p[1])
+	p_g[p_str] = np.sum(np.mean(np.array([gdict['splithalf'][gi]['g_diff'][idx] for gi in range(nsh)])) > gshuff)/len(gshuff)
 
 
 # e diff plot
 sns.set_palette(colors_age)
 xticks = [str(int(round(eqbins[i])))+\
 		  ' - '+str(int(round(eqbins[i+1])))+' y.o.' for i in range(len(eqbins)-1)]
-df = pd.DataFrame(columns=['Age', 's', 'ISC'])
+edf = pd.DataFrame(columns=['Age', 's', 'ISC'])
 for s in range(nsh):
 	for b in range(nbinseq):
-		df = df.append({'Age': xticks[b], 's': s, 'ISC': ISC_w[s,b]}, ignore_index=True)
+		edf = edf.append({'Age': xticks[b], 's': s, 'ISC': ISC_w[2,s,b]}, ignore_index=True)
 
 fig, ax = plt.subplots(figsize=(5,5))
-sns.swarmplot(x='Age',y='ISC',data=df,zorder=1)
-sns.pointplot(x="Age", y="ISC", data=df,markers='+',join=False,color='k',capsize=.1, zorder=100)
+sns.swarmplot(x='Age',y='ISC',data=edf,zorder=1)
+sns.pointplot(x="Age", y="ISC", data=edf,markers='+',join=False,color='k',capsize=.1, zorder=100)
 plt.setp(ax.get_xticklabels(), rotation=30, ha="right", fontsize = 15)
 plt.tight_layout()
 plt.savefig(figurepath+'HPC/SH_ISC_w_dist.png', bbox_inches='tight')
@@ -124,7 +164,7 @@ for event in event_list:
 			ISCedict['Age'].extend([xticks[b]]*len(x))
 			ISCedict['Event'].extend([esec]*len(x))
 			ISCedict['s'].extend([s]*len(x))
-			ISCedict['ISC'].extend(ISC_w_time[s,b,event-TW//2:event+TW//2])
+			ISCedict['ISC'].extend(ISC_w_time[2,s,b,event-TW//2:event+TW//2])
 			ISCedict['Time'].extend(x)
 dfe = pd.DataFrame(data=ISCedict)
 sns.set(font_scale = 2)
@@ -157,7 +197,7 @@ for event in event_list:
 			ISCgdict['Age pair'].extend([xticks[p[0]]+' with '+xticks[p[1]]]*len(x))
 			ISCgdict['Event'].extend([esec]*len(x))
 			ISCgdict['s'].extend([s]*len(x))
-			ISCgdict['ISC'].extend(ISC_g_time[s][str(p[0])+'_'+str(p[1])][event-TW//2:event+TW//2])
+			ISCgdict['ISC'].extend(ISC_g_time['splithalf'][s][str(p[0])+'_'+str(p[1])][event-TW//2:event+TW//2])
 			ISCgdict['Time'].extend(x)
 dfg = pd.DataFrame(data=ISCgdict)
 dfg = dfg[dfg['Age pair'].str.contains('16 - 19')]
@@ -197,11 +237,12 @@ for p in itertools.combinations(range(nbinseq),2):
 		for s in range(nsh):
 			ISCgtimedict['Age pair'].extend([xticks[p[0]]+' with '+xticks[p[1]]]*len(xstim))
 			ISCgtimedict['s'].extend([s]*len(xstim))
-			ISCgtimedict['ISC'].extend(ISC_g_time[s][str(p[0])+'_'+str(p[1])])
-			ISCgtimedict['z-scored ISC'].extend(zscore(ISC_g_time[s][str(p[0])+'_'+str(p[1])]))
+			ISCgtimedict['ISC'].extend(ISC_g_time['splithalf'][s][str(p[0])+'_'+str(p[1])])
+			ISCgtimedict['z-scored ISC'].extend(zscore(ISC_g_time['splithalf'][s][str(p[0])+'_'+str(p[1])]))
 			ISCgtimedict['Time'].extend(xstim)
 dfgtime = pd.DataFrame(data=ISCgtimedict)
 
+sns.set_palette(colors_age)
 fig,axes = plt.subplots(2,1,figsize=(20,5),sharex=True)
 ISCls = ['ISC','z-scored ISC']
 ymax = [20,1]
@@ -222,8 +263,8 @@ for b in range(nbinseq):
 	for s in range(nsh):
 		ISCetimedict['Age'].extend([xticks[b]]*len(xstim))
 		ISCetimedict['s'].extend([s]*len(xstim))
-		ISCetimedict['ISC'].extend(ISC_w_time[s,b])
-		ISCetimedict['z-scored ISC'].extend(zscore(ISC_w_time[s,b]))
+		ISCetimedict['ISC'].extend(ISC_w_time[2,s,b])
+		ISCetimedict['z-scored ISC'].extend(zscore(ISC_w_time[2,s,b]))
 		ISCetimedict['Time'].extend(xstim)
 dfetime = pd.DataFrame(data=ISCetimedict)
 
@@ -249,8 +290,8 @@ p2 = (3,4)
 for p1 in itertools.combinations(range(nbinseq),2):
 	if max(range(nbinseq)) in p1 and p1!=p2:
 		for s in range(nsh):
-			ISCp1 = ISC_g_time[s][str(p1[0])+'_'+str(p1[1])]
-			ISCp2 = ISC_g_time[s][str(p2[0])+'_'+str(p2[1])]
+			ISCp1 = ISC_g_time['splithalf'][s][str(p1[0])+'_'+str(p1[1])]
+			ISCp2 = ISC_g_time['splithalf'][s][str(p2[0])+'_'+str(p2[1])]
 			xcorr = np.correlate(ISCp1,ISCp2,"full")
 			glagdict['Age Pairs'].extend([xticks[p1[0]]+' with '+xticks[p1[1]]+' and '+xticks[p2[0]]+' with '+xticks[p2[1]]]*len(xcorrx))
 			glagdict['s'].extend([s]*len(xcorrx))
@@ -267,7 +308,7 @@ wlagdict = {'Age Pair':[],'correlation':[],'Time lag [s]':[],'s':[]}
 b2 = nbinseq-1
 for b1 in range(nbinseq-1):
 	for s in range(nsh):
-		xcorr = np.correlate(ISC_w_time[s,b1],ISC_w_time[s,b2],"full")
+		xcorr = np.correlate(ISC_w_time[2,s,b1],ISC_w_time[2,s,b2],"full")
 		wlagdict['Age Pair'].extend([xticks[b1]+' with '+xticks[b2]]*len(xcorrx))
 		wlagdict['s'].extend([s]*len(xcorrx))
 		wlagdict['correlation'].extend(xcorr/np.max(xcorr))
@@ -285,7 +326,7 @@ plt.savefig(figurepath+'HPC/ISC_xcorr_within.png', bbox_inches='tight')
 plt.rcParams.update({'font.size': 15})
 mask = np.zeros((nbinseq,nbinseq))
 mask[np.triu_indices_from(mask)] = True
-g_diff_mean = {'Age1':gdict[0]['Age1'],'Age2':gdict[0]['Age2'],'g_diff':np.mean(np.array([gdict[gi]['g_diff'] for gi in range(nsh)]),axis=0)}
+g_diff_mean = {'Age1':gdict['splithalf'][0]['Age1'],'Age2':gdict['splithalf'][0]['Age2'],'g_diff':np.mean(np.array([gdict['splithalf'][gi]['g_diff'] for gi in range(nsh)]),axis=0)}
 df = pd.DataFrame(data=g_diff_mean).pivot("Age1", "Age2", "g_diff")
 cols = df.columns.tolist()
 df = df[cols[-2:]+cols[:-2]]
