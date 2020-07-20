0292b54636c9433da3bf611170b41a343d46c2ca
Samantha Cohen <samantha.s.cohen@gmail.com>
2020-07-20 12:02:23 -0400

    Added g_time and e_time to g_ISC_sh_yeo.

--- 6_ISC_sh_yeo.py
+++ 6_ISC_sh_yeo.py
@@ -2,7 +2,6 @@
 
 import glob
 import tqdm
-import random
 import numpy as np
 import deepdish as dd
 from scipy.stats import zscore
@@ -10,17 +9,10 @@ from ISC_settings import *
 
 nTR=[750,250]
 bins = [0,4]
+nbins = len(nbins)
 HMMdir = HMMpath+'shuff/'
 savedir = ISCpath+'shuff_Yeo/'
 nsub = 41
-
-def ISCe_calc(roidict,task,shuffstr):
-	ISCe = roidict[task][shuffstr]['ISC_w'][1]-roidict[task][shuffstr]['ISC_w'][0]
-	return ISCe
-def ISCg_calc(roidict,task,shuffstr):
-    ISCg = sum(roidict[task][shuffstr]['ISC_b'])/4/(np.sqrt(roidict[task][shuffstr]['ISC_w'][1])*
-				   np.sqrt(roidict[task][shuffstr]['ISC_w'][0]))
-    return ISCg
 
 for roi in tqdm.tqdm(glob.glob(HMMdir+'*.h5')):
 	roi_short = roi.split('/')[-1][:-3]
@@ -29,7 +21,6 @@ for roi in tqdm.tqdm(glob.glob(HMMdir+'*.h5')):
 	for ti,task in enumerate(['DM','TP']):
 		roidict[task] = {}
 		vall = dd.io.load(roi,'/'+'/'.join([task,'bin_0','vall']))
-		roidict[task]['vall'] = vall
 		n_vox = len(vall)
 		n_time = nTR[ti]
 		D = []
@@ -42,11 +33,10 @@ for roi in tqdm.tqdm(glob.glob(HMMdir+'*.h5')):
 			Age.extend([bi]*len(subl))
 			D.append(dd.io.load(roi,'/'+'/'.join([task,bstr,'D'])))
 		D = np.concatenate(D)
+		roidict[task] = {'vall':vall, 'ISC_w':np.zeros((nshuff+1,nbins,n_vox)), 'ISC_w_time':np.zeros((nshuff+1,nbins,n_vox,n_time)), 'ISC_g_time':np.zeros((nshuff+1,n_vox,n_time)), 'ISC_g':np.zeros((nshuff+1,n_vox)), 'ISC_e':np.zeros((nshuff+1,n_vox))}
 		for shuff in range(nshuff+1):
-			shuffstr = 'shuff_'+str(shuff)
-			roidict[task][shuffstr] = {'ISC_w':[],'ISC_b':[]}
 			subh = even_out(Age,Sex)
-			groups = np.zeros((2,2,n_vox,n_time),dtype='float16')
+			groups = np.zeros((nbins,2,n_vox,n_time),dtype='float16')
 			for h in [0,1]:
 				for htmp in [0,1]:
 					group = np.zeros((n_vox,n_time),dtype='float16')
@@ -56,15 +46,24 @@ for roi in tqdm.tqdm(glob.glob(HMMdir+'*.h5')):
 						nanverts = np.argwhere(np.isnan(D[i,:]))
 						groupn[nanverts[:, 0],nanverts[:,1]] = groupn[nanverts[:,0],nanverts[:,1]]-1
 					groups[h,htmp] = zscore(group/groupn,axis=1)
-				roidict[task][shuffstr]['ISC_w'].append(np.sum(np.multiply(groups[h,0],groups[h,1]),axis=1)/(n_time-1))
+				roidict[task]['ISC_w_time'][shuff,h] = np.multiply(groups[h,0],groups[h,1])
+				roidict[task]['ISC_w'][shuff,h] = np.sum(roidict[task]['ISC_w_time'][shuff,h],axis=1)/(n_time-1)
+			ISC_b_time = []
 			for htmp1 in [0,1]:
 				for htmp2 in [0,1]:
-					roidict[task][shuffstr]['ISC_b'].append(np.sum(np.multiply(groups[0,htmp1],groups[1,htmp2]),axis=1)/(n_time-1))
+					ISC_b_time.append(np.multiply(groups[0,htmp1], groups[1,htmp2]))
 			# Now calculate g_diff and e_diff
-			roidict[task][shuffstr]['e_diff'] = ISCe_calc(roidict,task,shuffstr)
-			roidict[task][shuffstr]['g_diff'] = ISCg_calc(roidict,task,shuffstr)
-			# Now shuffle Age:
-			random.shuffle(Age)
+			ISCg_time = np.sum(ISC_b_time,axis=0)
+			denom = np.sqrt(roidict[task]['ISC_w'][shuff,0]) * \
+					np.sqrt(roidict[task]['ISC_w'][shuff,1])
+			roidict[task]['ISC_g_time'][shuff] = np.sum(ISC_b_time, axis=0)/4/np.tile(denom,(n_time,1)).T
+			roidict[task]['ISC_g'][shuff] = np.sum([np.sum(i,axis=1)/(n_time-1) for i in ISC_b_time], axis=0)/4/denom
+			roidict[task]['ISC_e'][shuff] = roidict[task]['ISC_w'][shuff,0] - \
+					roidict[task]['ISC_w'][shuff,1]
+			# Now shuffle Age, and Sex in same order:
+			neword = np.random.permutation(len(Age))
+			Age = Age[neword]
+			Sex = Sex[neword]
 	dd.io.save(savedir+roi_short+'.h5',roidict)
 				
 				
