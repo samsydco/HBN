55fc1b3e0eee042f04a3429e063ddf87d52570e4
Samantha Cohen <samantha.s.cohen@gmail.com>
2020-09-03 13:04:44 -0400

    Edits to 7_ISC_up_down: now using matplotlib instead of seaborn for plotting.

--- 7_ISC_up_down.py
+++ 7_ISC_up_down.py
@@ -34,6 +34,7 @@ def p_calc(ISC,ISCtype='e'):
 		p = np.sum(np.nanmean(ISC[0])>np.nanmean(ISC[1:],axis=1))/nshuff
 	return p,nshuff
 
+plt.rcParams.update({'font.size': 20})
 for roi in tqdm.tqdm(ROIl):
 	roi_short = roi.split('/')[-1][:-3]
 	roidict = dd.io.load(roi)
@@ -69,25 +70,21 @@ for roi in tqdm.tqdm(ROIl):
 					ISC_w[b] = roidict[task]['ISC_w'][0,idx]
 			roidict[task]['ISC_w_all'] = ISC_w
 			dd.io.save(roi,roidict)
-		fdict = {'Age':[],'ISC':[],'vox':[],'corr':[],'null':[]}
-		for vi in range(len(vall)):
-			for b in bins:
-				fdict['null'].append(np.mean(roidict[task]['ISC_w'][1:,:,vi]))
-				fdict['Age'].append(xticks[b])
-				fdict['ISC'].append(ISC_w[b,vi])
-				fdict['vox'].append(vi)
-				fdict['corr'].append(np.corrcoef([e+agespan/2 for e in eqbins[:-1]],ISC_w[:,vi])[0,1])
-		color = 'tab:red' if np.mean(fdict['corr'])>0 else 'royalblue'
-		nullmean = np.mean(fdict['null'])
-		nullstd = np.std(fdict['null'],ddof=1).mean()
-		df = pd.DataFrame(data=fdict)
-		fig = plt.fill_between(np.arange(nbinseq+1)-0.5,
+		nullmean = np.mean(roidict[task]['ISC_w'][1:])
+		nullstd = np.std(np.mean(np.mean(roidict[task]['ISC_w'][1:],axis=1),axis=0))
+		fig,ax = plt.subplots()
+		ax.fill_between(np.arange(nbinseq+1)-0.5,
 						nullmean-nullstd, nullmean+nullstd,
 						alpha=0.2, edgecolor='none', facecolor='grey')
-		#fig = sns.swarmplot(x="Age", y="ISC", data=df,zorder=1,color=color)
-		fig = sns.pointplot(x="Age", y="ISC", data=df,markers='+',join=False,color='k',ci='sd',capsize=.1, zorder=100)
-		plt.xticks(rotation=30,ha="right")
-		plt.tight_layout()
+		ax.axes.errorbar(np.arange(len(xticks)),
+						 np.mean(ISC_w,axis=1), 
+						 yerr = np.std(ISC_w,axis=1), 
+						 xerr = None, ls='none',capsize=10, elinewidth=1,fmt='.k',
+						 markeredgewidth=1) 
+		ax.set_xticks(np.arange(len(xticks)))
+		ax.set_xticklabels(xticks,rotation=45, fontsize=20)
+		ax.set_xlabel('Age',labelpad=20, fontsize=20)
+		ax.set_ylabel('ISC',labelpad=20, fontsize=20)
 		plt.show()
 		b1mean = np.mean(df.loc[df['Age']==xticks[0]]['ISC'])
 		sig = '*' if nullmean-nullstd>b1mean or nullmean+nullstd<b1mean else ''
