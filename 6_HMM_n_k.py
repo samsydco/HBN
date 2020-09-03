b29e1e83db73cae46fff57838af87eb3dfe81c63
Samantha Cohen <samantha.s.cohen@gmail.com>
2020-09-03 13:53:23 -0400

    Adding figures to 6_HMM_n_k.

--- 6_HMM_n_k.py
+++ 6_HMM_n_k.py
@@ -40,6 +40,34 @@ for roi in tqdm.tqdm(glob.glob(sametimedir+'*h5')):
 			roidict[task][b]['tune_ll'] = tune_ll
 			roidict[task][b]['best_k'] = k_list[np.argmax(np.mean(tune_ll,0))]
 	dd.io.save(nkdir+roi_short+'.h5',roidict)
+	
+task='DM'
+roidict = {k:{k:[] for k in ['k','ll','roi']} for k in ['0','4']}
+for roi in tqdm.tqdm(glob.glob(nkdir+'*.h5')):
+	roi_short = roi.split('/')[-1][:-3]
+	data = dd.io.load(roi,'/'+task)
+	for b in bins:
+		roidict[str(b)]['roi'].append(roi_short)
+		roidict[str(b)]['k'].append(data[b]['best_k'])
+		kidx = np.where(k_list==data[b]['best_k'])[0][0]
+		roidict[str(b)]['ll'].append(np.mean(data[b]['tune_ll'][:,kidx]))
+
+import matplotlib.pyplot as plt
+xticks = [str(int(round(eqbins[b])))+' - '+str(int(round(eqbins[b+1])))+' y.o.' for b in bins]
+for val in ['k','ll']:
+	fig, ax = plt.subplots(figsize=(20,20))
+	ax.scatter(roidict['0'][val], roidict['4'][val])
+	minval = np.min(roidict['0'][val])
+	maxval = np.max(roidict['0'][val])
+	ax.plot([minval,maxval],\
+			 [minval,maxval], 'k--', linewidth=2)
+	ax.set_xlabel(xticks[0]+' '+val+'\'s')
+	ax.set_ylabel(xticks[1]+' '+val+'\'s')
+	for i, txt in enumerate(roidict['0']['roi']):
+		ax.annotate(txt, (roidict['0'][val][i], roidict['4'][val][i]))
+		
+		
+	
 		
 					
 	
