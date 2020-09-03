c38b02af3a0998501f024396bf6b039b1276a841
Samantha Cohen <samantha.s.cohen@gmail.com>
2020-09-03 14:21:01 -0400

    6_HMM_n_k figures.

--- 6_HMM_n_k.py
+++ 6_HMM_n_k.py
@@ -53,18 +53,43 @@ for roi in tqdm.tqdm(glob.glob(nkdir+'*.h5')):
 		roidict[str(b)]['ll'].append(np.mean(data[b]['tune_ll'][:,kidx]))
 
 import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
+grey=211/256
+plt.rcParams.update({'font.size': 20})
 xticks = [str(int(round(eqbins[b])))+' - '+str(int(round(eqbins[b+1])))+' y.o.' for b in bins]
-for val in ['k','ll']:
-	fig, ax = plt.subplots(figsize=(20,20))
-	ax.scatter(roidict['0'][val], roidict['4'][val])
-	minval = np.min(roidict['0'][val])
-	maxval = np.max(roidict['0'][val])
-	ax.plot([minval,maxval],\
-			 [minval,maxval], 'k--', linewidth=2)
-	ax.set_xlabel(xticks[0]+' '+val+'\'s')
-	ax.set_ylabel(xticks[1]+' '+val+'\'s')
-	for i, txt in enumerate(roidict['0']['roi']):
-		ax.annotate(txt, (roidict['0'][val][i], roidict['4'][val][i]))
+for lab in ['_lab','']:
+	for val in ['k','ll']:
+		r,p=pearsonr(roidict['0'][val], roidict['4'][val])
+		fig, ax = plt.subplots(figsize=(15,15))
+		ax.set_title('r:'+str(np.round(r,2))+', p:'+str(np.round(p,2)))
+		minval = np.min(roidict['0'][val])
+		maxval = np.max(roidict['0'][val])
+		ax.plot([minval,maxval],\
+				 [minval,maxval], 'k--', linewidth=2)
+		ax.set_xlabel(xticks[0]+' '+val+'\'s')
+		ax.set_ylabel(xticks[1]+' '+val+'\'s')
+		if lab == '_lab':
+			for i, txt in enumerate(roidict['0']['roi']):
+				if roidict['0'][val][i]>roidict['4'][val][i]:
+					ax.annotate('\n'.join(txt.split('_')[:-1]), 
+								(roidict['0'][val][i], roidict['4'][val][i]),
+							horizontalalignment="left",
+							verticalalignment="top",color=(grey,grey,grey))
+				if roidict['0'][val][i]<roidict['4'][val][i]:
+					ax.annotate('\n'.join(txt.split('_')[:-1]), 
+								(roidict['0'][val][i], roidict['4'][val][i]),
+							horizontalalignment="right",
+							verticalalignment="bottom",color=(grey,grey,grey))
+		ax.scatter(roidict['0'][val], roidict['4'][val])
+		fig.savefig(figurepath+'n_k/'+val+lab+'.png')
+		
+		
+			
+			
+			or roidict['4'][val][i]*1.25>roidict['0'][val][i]:
+			
+		
+	fig.savefig(figurepath+'n_k/'+val+'.png')
 		
 		
 	
