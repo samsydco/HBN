7fd88164e74f7250967c5108451df3ba9f3620ea
Samantha Cohen <samantha.s.cohen@gmail.com>
2020-09-16 14:20:43 -0400

    Added zoomed in plots to 8_HMM_Caroline_2.

--- 8_HMM_Caroline_2.py
+++ 8_HMM_Caroline_2.py
@@ -17,15 +17,22 @@ nbins = len(bins)
 lgd = [str(int(round(eqbins[b])))+' - '+str(int(round(eqbins[b+1])))+' y.o.' for b in bins]
 colors = ['#edf8fb','#b3cde3','#8c96c6','#8856a7','#810f7c']
 grey = 211/256 # other options: https://www.rapidtables.com/web/color/gray-color.html
+ztw = 200
+
+qrois = ['LH_LimbicA_TempPole_2',
+ 'LH_VisCent_ExStr_1',
+ 'LH_VisCent_Striate_1',
+ 'LH_VisPeri_ExStrInf_1',
+ 'RH_DorsAttnA_TempOcc_1']
+ROIl = [HMMdir+roi+'.h5' for roi in qrois]
 
 task='DM'
 nTR_ = nTR[0]
-for roi in tqdm.tqdm(glob.glob(HMMdir+'*.h5')):
+for roi in tqdm.tqdm(ROIl):
 	roi_short = roi.split('/')[-1][:-3]
-	HMMtask = dd.io.load(roi,'/'+task)
-	nshuff = len([k for k in list(HMMtask.keys()) if 'shuff' in k]) - 1
-	p_auc = np.sum(abs(HMMtask['shuff_0']['auc_diff'])<[abs(HMMtask['shuff_'+str(s)]['auc_diff']) for s in range(1,nshuff+1)])/nshuff
+	p_auc = dd.io.load(ISCpath+'p_vals.h5','/'+roi_short+'/auc_diff/p')
 	if p_auc<0.05 or 'LH_DefaultB_PFCd_1' in roi:
+		HMMtask = dd.io.load(roi,'/'+task)
 		time = np.arange(TR,nTR_*TR+1,TR)[:-1]
 		k = HMMtask['best_k']
 		D = [HMMtask['bin_'+str(b)]['D'] for b in bins]
@@ -36,7 +43,7 @@ for roi in tqdm.tqdm(glob.glob(HMMdir+'*.h5')):
 		fig, ax = plt.subplots(figsize=(10, 10))
 		ax.set_xticks(np.append(time[0::nTR_//5],time[-1]))
 		ax.set_xticklabels([str(int(s//60))+':'+str(int(s%60)) for s in time][0::nTR_//5]+['10:00'], fontsize=30)
-		ax.set_xlabel('Time (seconds)', fontsize=35)
+		ax.set_xlabel('Time (minutes)', fontsize=35)
 		klax = kl if k < 25 else kl[4::5]
 		ax.set_yticks(klax)
 		ax.set_yticklabels(klax,fontsize=30)
@@ -57,11 +64,28 @@ for roi in tqdm.tqdm(glob.glob(HMMdir+'*.h5')):
 		else:
 			ax.set_facecolor((grey,grey,grey))
 			plt.savefig(figurepath+'HMM/auc_FLUX/'+roi_short+'.png', bbox_inches='tight')
+		#zoomed figure
+		timez=time[:ztw]
+		fig, ax = plt.subplots(figsize=(10, 10))
+		ax.set_xticks(np.append(timez[0::nTR_//5],time[-1]))
+		ax.set_xticklabels([str(int(s//60))+':'+str(int(s%60)) for s in timez][0::nTR_//5]+['10:00'], fontsize=30)
+		ax.set_xlabel('Time (minutes)', fontsize=35)
+		ax.set_yticks(klax)
+		ax.set_yticklabels(klax,fontsize=30)
+		ax.set_ylabel('Events', fontsize=45)
+		for bi in range(len(bins)):
+			ax.plot(timez, E_k[bi][:ztw], linewidth=5.5, alpha=0.5, color=colors[bi],label=lgd[bi])
+		if 'LH_DefaultB_PFCd_1' in roi:
+			ax.legend(prop={'size': 30},facecolor=(grey,grey,grey),edgecolor='black')
+			plt.savefig(figurepath+'HMM/auc_FLUX/legend_zoom.png', bbox_inches='tight')
+		else:
+			ax.set_facecolor((grey,grey,grey))
+			plt.savefig(figurepath+'HMM/auc_FLUX/'+roi_short+'_zoom.png', bbox_inches='tight')
 			
 #roi_example
 # display low vs high ll
 # display idealized squiggle plot
-k = 5
+k = 3
 kl = np.arange(k)+1
 time = np.arange(TR,nTR_*TR+1,TR)[:-1]
 ev_durr = [200,100,175,50,225]
