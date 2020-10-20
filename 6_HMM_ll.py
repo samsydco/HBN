67570dd8452bc447a0bc436bb0c09d5de26f728e
Samantha Cohen <samantha.s.cohen@gmail.com>
2020-10-20 14:59:08 -0400

    Edited 6_HMM_ll - added figure.

--- 6_HMM_ll.py
+++ 6_HMM_ll.py
@@ -29,19 +29,31 @@ for roi in glob.glob(HMMdir+'*.h5'):
 		lldict[roi_short][str(b)+'_ll_min_opt'] = np.min(ll[b,:,idx])/nTR
 		lldict[roi_short][str(b)+'_bestk'] = np.mean([k_list[np.argmax(ll[b,ki])] for ki in range(nsplit)])
 		lldict[roi_short][str(b)+'_worstk'] = np.mean([k_list[np.argmin(ll[b,ki])] for ki in range(nsplit)])
+		lldict[roi_short][str(b)+'_2k'] = np.mean(ll[b,:,0],axis=0)/nTR
+		lldict[roi_short][str(b)+'_2k_diff'] = lldict[roi_short][str(b)+'_ll_max'] - lldict[roi_short][str(b)+'_2k']
 		
 dd.io.save(HMMpath+'ll_diff.h5',lldict)
 lldict = dd.io.load(HMMpath+'ll_diff.h5')
 
 import pandas as pd
 df=pd.DataFrame(lldict).T
+df=df.drop(columns=['0_worstk','4_worstk','0_ll_diff','4_ll_diff','0_bestk','4_bestk',
+				   '0_ll_min_opt','4_ll_min_opt','0_ll_max','4_ll_max','0_2k','4_2k'])
 
 import matplotlib.pyplot as plt
+import scipy.stats as stats
 plt.rcParams.update({'font.size': 40})
-for comp in ['_ll_diff','_ll_min_opt']:
+thresh = 0.005
+for comp in ['_2k_diff']:#['_ll_diff','_ll_min_opt','_2k']:
+	excllist=df[((df['0'+comp]<thresh) | (df['4'+comp]<thresh))].index.values.tolist()
 	x = df['0'+comp]; y = df['4'+comp]
+	idx1 = np.unique(np.concatenate((np.where(y<thresh)[0],np.where(x<thresh)[0])))
+	idx2 = np.intersect1d(np.where(y>thresh)[0],np.where(x>thresh)[0])
+	x1 = x.iloc[idx1]; y1 = y.iloc[idx1]
+	x2 = x.iloc[idx2]; y2 = y.iloc[idx2]
 	fig, ax = plt.subplots(figsize=(15,15))
-	ax.scatter(x,y,s=100,alpha=0.25)
+	ax.scatter(x1,y1,s=100,alpha=0.25)
+	ax.scatter(x2,y2,s=100,alpha=0.25)
 	ax.set_xlim([np.min(x)-0.005,np.max(x)+0.005])
 	ax.set_ylim([np.min(y)-0.005,np.max(y)+0.005])
 	ax.set_xlabel('Youngest '+comp[1:])
