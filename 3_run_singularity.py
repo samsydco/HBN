6713d1dde6727a9d2f7b1e78dda4d64339b6909e
Samantha Cohen <samantha.s.cohen@gmail.com>
2020-11-02 16:47:02 -0500

    edits made to 3_run_singularity.py

--- 3_run_singularity.py
+++ 3_run_singularity.py
@@ -11,18 +11,6 @@ for attr in attrlist:
 
 path_tmp = path+site+'/'
 
-'''
-plist = []
-for f in glob.glob(outputdr+'/fmriprep/sub*/figures/'):
-	if len(glob.glob(f+'*')) < 15:
-		plist.append(f.split('/')[-3])
-nchunk = 4 # number of items per chunk (maybe use 10?)
-pchunk = [plist[x:x+nchunk] for x in range(0,len(plist),nchunk)]
-# https://neurostars.org/t/using-use-syn-sdc-with-fieldmap-data/2592
-# This works: --use-syn-sdc --ignore fieldmaps \
-'''
-
-
 for chunk in tqdm(pchunk):
 	pstr = ' '.join([c[4:] for c in chunk])
 	for task in ['DM','TP']:
