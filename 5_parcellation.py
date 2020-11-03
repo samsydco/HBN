da1091a0367ffc8e07037ac675f5945c1d8ab47a
Samantha Cohen <samantha.s.cohen@gmail.com>
2020-11-03 16:24:00 -0500

    Created 5_parcellation.py - parcellation file.

---
+++ 5_parcellation.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# make h5 files for each parcel
+# data = subjects x verticies x TRs
+
+import nibabel.freesurfer.io as free
+from HMM_settings import *
+
+tasks=['DM']
+bins = np.arange(nbinseq)
+savepath = 
+
+for hemi in glob.glob(path+'ROIs/annot/*'):
+	print(hemi)
+	lab = free.read_annot(hemi)
+	for ri,roi_tmp in enumerate(lab[2]):
+		roi=roi_tmp.decode("utf-8")
+		roi_short=roi_tmp.decode("utf-8")[11:]
+		roidict = {}
+		vall = np.where(lab[0]==ri)[0]
+		roidict['hemi'] = roi_short[0]
+		for ti,task in enumerate(tasks):
+			roidict[task] = {}
+			nTR_ = nTR[ti]
+			for b in bins:
+				roidict[task]['bin_'+str(b)] = {}
+				subl = [ageeq[i][1][b][idx] for i in [0,1] for idx in np.random.choice(lenageeq[i][b],minageeq[i],replace=False)]
+				roidict[task]['bin_'+str(b)]['subl'] = subl
+				nsub = len(subl)
+				# Load data
+				D = np.empty((nsub,len(vall),nTR_),dtype='float16')
+				badvox = []
+				for sidx, sub in enumerate(subl):
+					D[sidx,:,:] = dd.io.load(sub,['/'+task+'/'+roidict['hemi']],sel=dd.aslice[vall,:])[0]
+					badvox.extend(np.where(np.isnan(D[sidx,:,0]))[0]) # Some subjects missing some voxels
+				D = np.delete(D,badvox,1)
+				vall = np.delete(vall,badvox)
+				roidict['vall'] = vall
+				roidict[task]['bin_'+str(b)]['D'] = D
+		if len(vall) > 0:
+			dd.io.save(ISCpath+roi_short+'.h5',roidict)
+			
