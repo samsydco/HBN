9a9eaa6cf04c954b255b82fb66f19c5305863477
Samantha Cohen <samantha.s.cohen@gmail.com>
2020-11-03 14:19:08 -0500

    Modified event_ratings - cleaned up non-used code.

--- event_ratings.py
+++ event_ratings.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 import numpy as np
 import pandas as pd
 import deepdish as dd
@@ -27,8 +26,6 @@ def xcorr(a,b):
 	c = np.correlate(a, b, 'full')/max(len(a),len(b))
 	return c
 
-
-
 eventdict = {key:{} for key in ['timing','annotation']}
 for csv in glob.glob(segpath+'*csv'):
 	initials = csv.split('/')[-1].split('-')[0]
@@ -48,7 +45,6 @@ nsubj = len(eventdict['timing'])
 
 sexl=['DA:F','SB:F','AR:F','FBM:M','TM:F','MT:F','MJ:F','TK:M','NR:M','CG:F','MN:M','EG:F','HC:F','HDZ:F', 'MB:M', 'SJ:F', 'MCK:F', 'FF:F', 'AS:M', 'DS:F', 'TC:F']
 
-
 nevent = []
 ev_annot = []
 for v in eventdict['timing'].values():
@@ -59,34 +55,14 @@ ev_annot = np.asarray(ev_annot, dtype=int)
 counts = np.append(np.bincount(ev_annot)[:-2],np.bincount(ev_annot)[-1])
 ev_conv = np.convolve(counts,hrf)[:nTR]
 
-peaks = np.where(ev_conv>4)[0]
-
-peakdiff = [0]+list(np.where(np.diff(peaks)>1)[0]+1)+[len(peaks)]
-
-event_list = []
-event_peak = []
-for pi,pdi in enumerate(peakdiff[:-1]):
-	seg = peaks[pdi:peakdiff[pi+1]]
-	event_list.append(seg[np.argmax(ev_conv[seg])])
-	event_peak.append(np.max(ev_conv[seg]))
-evidx = np.argsort(event_peak)[-1:int(len(event_peak)-np.round(np.median(nevent)))-1:-1]
-event_list = [e for ei,e in enumerate(event_list) if ei in evidx]
-event_peak = [e for ei,e in enumerate(event_peak) if ei in evidx]
-
-event_annot = [[]for e in event_list]
-for ei,e in enumerate(event_list):
-	for sub in eventdict['timing'].keys():
-		for ti,t in enumerate(eventdict['timing'][sub]):
-			if e-3<t and t<e+3:
-				event_annot[ei].append(eventdict['annotation'][sub][ti])
-
 if __name__ == "__main__":
 	
+	# Tally of annotations at each time point
 	time = np.arange(0,nTR)
 	fig, (raw_ev_annot) = plt.subplots(figsize=(60, 20))
-	counts = raw_ev_annot.hist(ev_annot, bins=nTR, linewidth=20,color='k')#,alpha=0.25)
+	raw_ev_annot.hist(ev_annot, bins=nTR, linewidth=20,color='k')
 
-	yticks = list(np.arange(0, max(counts[0])+1, 2))
+	yticks = list(np.arange(0, max(counts)+1, 2))
 	raw_ev_annot.spines['right'].set_color('none')
 	raw_ev_annot.spines['top'].set_color('none')
 	raw_ev_annot.set_xticks(np.append(time[0::nTR//5],time[-1]))
@@ -99,9 +75,7 @@ if __name__ == "__main__":
 	plt.tight_layout()
 	plt.savefig(ev_figpath+'ev_annots_hist.png', bbox_inches='tight')
 
-
-
-
+	# counts convolved with HRF
 	fig, (hrf_ann) = plt.subplots(figsize=(60, 20))
 	hrf_ann.plot(np.arange(nTR), ev_conv, linewidth=5,color='k')
 	hrf_ann.spines['right'].set_color('none')
@@ -111,21 +85,10 @@ if __name__ == "__main__":
 	hrf_ann.set_xlabel('TR (1 TR = 0.8 seconds)', fontsize=40)
 	plt.savefig(ev_figpath+'hrf_conv.png', bbox_inches='tight')
 	
-	from ISC_settings import *
-	D_ = dd.io.load(ISCpath+'HPC.h5',['/D'])[0]
-	D = np.zeros(nTR)
-	nsubj = 0
-	for b in range(nbinseq):
-		for bumps in D_[b].values():
-			D=D+bumps
-			nsubj+=1
-	D=D/nsubj
-
-
+	# illustration of counts with HRF-covlved counts
 	fig, (raw_ev_annot) = plt.subplots(figsize=(60, 20))
-	counts = raw_ev_annot.hist(ev_annot, bins=nTR, linewidth=20,color='k')
+	raw_ev_annot.hist(ev_annot, bins=nTR, linewidth=20,color='k')
 	raw_ev_annot.plot(np.arange(nTR), ev_conv, color='k',alpha=0.5,linewidth=5)
-	#raw_ev_annot.plot(event_list,event_peak,'b*',markersize=24)
 	raw_ev_annot.spines['right'].set_color('none')
 	raw_ev_annot.spines['top'].set_color('none')
 	raw_ev_annot.set_xticks(np.append(time[0::nTR//5],time[-1]))
@@ -138,12 +101,22 @@ if __name__ == "__main__":
 	plt.tight_layout()
 	plt.savefig(ev_figpath+'ev_annots_both.png', bbox_inches='tight')
 	
+	from ISC_settings import *
+	D_ = dd.io.load(ISCpath+'HPC.h5',['/D'])[0]
+	D = np.zeros(nTR)
+	nsubj = 0
+	for b in range(nbinseq):
+		for bumps in D_[b].values():
+			D=D+bumps
+			nsubj+=1
+	D=D/nsubj
+	
+	# average hippocampal activity overlaid with counts and ev_conv:
 	fig, (raw_ev_annot) = plt.subplots(figsize=(60, 20))
-	counts = raw_ev_annot.hist(ev_annot, bins=nTR, linewidth=20,color='k')
+	raw_ev_annot.hist(ev_annot, bins=nTR, linewidth=20,color='k')
 	raw_ev_annot.plot(np.arange(nTR), ev_conv, color='k',alpha=0.5,linewidth=5)
 	ax2 = raw_ev_annot.twinx()
 	ax2.plot(np.arange(nTR)[5:], D[5:], color='k',linewidth=10)
-	#raw_ev_annot.plot(event_list,event_peak,'b*',markersize=24)
 	raw_ev_annot.spines['right'].set_color('none')
 	raw_ev_annot.spines['top'].set_color('none')
 	raw_ev_annot.set_xticks(np.append(time[0::nTR//5],time[-1]))
@@ -156,20 +129,15 @@ if __name__ == "__main__":
 	plt.tight_layout()
 	plt.savefig(ev_figpath+'ev_annots_HPC.png', bbox_inches='tight')
 	plt.savefig(ev_figpath+'ev_annots_HPC.eps', bbox_inches='tight')
-	
 
-			
+	# average hippocampal activity	
 	fig, ax = plt.subplots(figsize=(60, 20))
 	ax.plot(np.arange(nTR)[5:], D[5:], color='k',linewidth=10)
-	#raw_ev_annot.plot(event_list,event_peak,'b*',markersize=24)
 	ax.spines['right'].set_color('none')
 	ax.spines['top'].set_color('none')
 	ax.set_xticks(np.append(time[0::nTR//5],time[-1]))
 	ax.set_xticklabels([str(int(s*TR//60)) +':'+ str(int(s*TR%60))+'0' for s in time][0::nTR//5]+['10:00'], fontsize=80)
-	#raw_ev_annot.set_yticks(yticks)
 	plt.rcParams['xtick.labelsize']=50
-	#plt.rcParams['ytick.labelsize']=80
-	#raw_ev_annot.set_ylabel('# annotations', fontsize=80)
 	ax.set_xlabel('Time (minutes)', fontsize=80)
 	plt.tight_layout()
 	plt.savefig(figurepath+'HPC/HPC_timecourse.png', bbox_inches='tight')
@@ -177,7 +145,6 @@ if __name__ == "__main__":
 	
 	# Compare annotations "ev_conv" with HPC bumps:
 	# Precidence for thinking this way comes from Aya Ben-Yakov's event saliency measure
-	from ISC_settings import *
 	sizedict = dd.io.load(ISCpath+'HPC_vol.h5')
 	sizedf = pd.DataFrame(data=sizedict)
 	import statsmodels.api as sm
@@ -198,7 +165,7 @@ if __name__ == "__main__":
 	Dall = {}
 	bumplagdict = {'Age':[],'Time lag [s]':[],'Subj':[],'Exact_Age':[]}
 	for HPC in ['HPC','aHPC','pHPC']:
-		D,ISC_w_time,ISC_g_time = dd.io.load(ISCpath+HPC+'.h5',['/D','/ISC_w_time', '/ISC_g_time'])
+		D = dd.io.load(ISCpath+HPC+'.h5','/D')
 		Dall[HPC] = D
 		bumplagdict['correlation_'+HPC] = []
 	
@@ -213,6 +180,7 @@ if __name__ == "__main__":
 	dfbumplag = pd.DataFrame(data=bumplagdict)
 	dfbumplag = dfbumplag[abs(dfbumplag['Time lag [s]'])<20]
 	
+	# Test for interaction between aHPC and pHPC correlation with ev_conv:
 	result = sm2.ols(formula='Exact_Age ~ correlation_aHPC + correlation_pHPC + correlation_aHPC * correlation_pHPC', data=dfbumplag).fit()
 	print(result.summary()) #no interaction!
 	
@@ -223,15 +191,15 @@ if __name__ == "__main__":
 	tvals = np.zeros(len(times))
 	pvals = np.zeros(len(times))
 	for ti,tp in enumerate(times):
-		tvals[ti],pvals[ti] = stats.ttest_1samp(dfpost[dfpost['Time lag [s]']==tp]['correlation_HPC'], 0)#stats.ttest_rel(dfpost[dfpost['Time lag [s]']==tp]['correlation'],dfpost[dfpost['Time lag [s]']==0.]['correlation'])
+		tvals[ti],pvals[ti] = stats.ttest_1samp(dfpost[dfpost['Time lag [s]']==tp]['correlation_HPC'],0)
 	pvals = pvals*len(pvals) # Bonferroni correction
 	best_t_i = times[np.argmin(pvals)]
 	best_t = 0 # Set from whole HPC
 	tempdf = dfpost[dfpost['Time lag [s]']==best_t]
 	tempsize = tempdf.merge(sizedf, on='Subj')
+	# test for linear vs polynomial fit of Age vs event_correlation:
+	# both line and U fit equally well!
 	for HPC in ['HPC','aHPC','pHPC']:
-		# test for linear vs polynomial fit of Age vs event_correlation:
-		# both line and U fit equally well!
 		sse = np.zeros((2,nsplit))
 		for degi,deg in enumerate([1,2]):
 			for split,Ls in enumerate(kf.split(np.arange(nsub),y)):
@@ -239,22 +207,16 @@ if __name__ == "__main__":
 									tempdf['correlation_'+HPC].iloc[Ls[0]],deg)
 				pred = np.polyval(coeffs, tempdf['Exact_Age'].iloc[Ls[1]])
 				sse[degi,split] = np.sum(np.square(tempdf['correlation_'+HPC].iloc[Ls[1]]-pred))
-			print('Sum of Squared Error for Age-to-'+HPC+'to event boundary correlation is: '+
+			print('Sum of Squared Error for Age-to-'+HPC+' to event boundary correlation is: '+
 				 str(np.round(np.mean(sse[degi]),3))+'+/-'+str(np.round(np.std(sse[degi]),3))+
 				 ' for degree '+str(deg)+'.')
 		t,p = stats.ttest_rel(sse[0],sse[1])
 		print(t,p)
 		
-		
-		
-		r,p = stats.pearsonr(tempdf['Exact_Age'],tempdf['correlation_'+HPC])
-		# no correlation between size and event-response!!
-		r2,p2 = stats.pearsonr(tempsize[HPC],tempsize['correlation_'+HPC])
-		
-		
+	for HPC in ['HPC','aHPC','pHPC']:
 		OLS_model = sm.OLS(tempsize['correlation'],tempsize[HPC]).fit()  # training the model
 		residual_values = OLS_model.resid # residual values
-		r,p = stats.pearsonr(tempsize['Exact_Age'],tempsize['correlation_'+HPC])
+		r3,p3 = stats.pearsonr(tempsize['Exact_Age'],tempsize['correlation_'+HPC])
 		
 		X = sm.add_constant(tempsize[['Exact_Age',HPC]]) # adding a constant
 		model = sm.OLS(tempsize['correlation_'+HPC], X).fit()
@@ -262,6 +224,8 @@ if __name__ == "__main__":
 		print_model = model.summary()
 		print(print_model)
 		
+	for HPC in ['HPC','aHPC','pHPC']:
+		r,p = stats.pearsonr(tempdf['Exact_Age'],tempdf['correlation_'+HPC])
 		sns.set(font_scale = 2,rc={'axes.facecolor':(grey,grey,grey)})
 		fig,ax=plt.subplots(figsize=(7,5))
 		sns.regplot(x='Exact_Age', y="correlation_"+HPC, data=tempdf,color=colors_age[3])#.set_title('Delay = '+str(best_t)+'s\nr = '+str(np.round(r,2))+', p = '+str(np.round(p,2)))
@@ -271,21 +235,21 @@ if __name__ == "__main__":
 		print(HPC,', r = '+str(np.round(r,2)),', p = '+str(np.round(p,5)))
 		fig.savefig(figurepath+'HPC/'+HPC+'_Age_vs_bump_xcorr_ev_conv.png', bbox_inches='tight', dpi=300)
 		
+	for HPC in ['HPC','aHPC','pHPC']:
+		# no correlation between size and event-response!!
+		r2,p2 = stats.pearsonr(tempsize[HPC],tempsize['correlation_'+HPC])
 		# plot indicating size
 		fig,ax=plt.subplots(figsize=(7,5))
-		sns.scatterplot(x='Exact Age', y="correlation_"+HPC, hue=HPC, size=HPC,
-                linewidth=0,alpha=.5, 
+		sns.scatterplot(x='Exact_Age', y="correlation_"+HPC, hue=HPC, size=HPC,linewidth=0,alpha=.5, 
                 data=tempsize, ax=ax)
-		#sns.relplot(x='Exact Age', y="correlation", hue=HPC, size=HPC,
-		#			alpha=.5, color=colors_age[3],#palette="purple",
-		#			data=tempsize)
 		ax.set_xlabel('Age')
 		ax.set_ylabel('Hippocampus-to-event\ncorrelation')
 		plt.rcParams['axes.xmargin'] = 0
 		ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-		print(HPC,', r = '+str(np.round(r,2)),', p = '+str(np.round(p,5)))
+		print(HPC,', r = '+str(np.round(r2,2)),', p = '+str(np.round(p2,5)))
 		fig.savefig(figurepath+'HPC/'+HPC+'_Age_vs_bump_size.png', bbox_inches='tight', dpi=300)
 		
+	for HPC in ['HPC','aHPC','pHPC']:
 		# plot all ages together
 		sns.set(font_scale = 2,rc={'axes.facecolor':(grey,grey,grey)})
 		sns.set_palette(colors_age)
@@ -293,11 +257,12 @@ if __name__ == "__main__":
 		g = sns.lineplot(x='Time lag [s]', y='correlation_'+HPC, ax=ax, data=dfbumplag, ci=95,color=colors_age[3])
 		ax.set_xlim([-10,10])
 		ax.set_xlabel('Time (seconds)')
-		ax.set_ylabel('Hippocampus-to-event\ncorrelation_'+HPC)
+		ax.set_ylabel('Hippocampus-to-event\ncorrelation '+HPC)
 		ax.margins(x=0)
 		plt.savefig(figurepath+'HPC/'+HPC+'_bump_all.png', bbox_inches='tight',dpi=300)
 	
-		# plot timecourse of xcorr with *'s for significance
+	for HPC in ['HPC','aHPC','pHPC']:
+		# plot timecourse of xcorr
 		sns.set(font_scale = 2,rc={'axes.facecolor':(grey,grey,grey)})
 		sns.set_palette(colors_age)
 		fig,ax = plt.subplots(1,1,figsize=(7,7))
@@ -310,43 +275,3 @@ if __name__ == "__main__":
 		ax.legend(loc='center', bbox_to_anchor=(0.5, -0.5))
 		ax.margins(x=0)
 		plt.savefig(figurepath+'HPC/'+HPC+'_bump_xcorr_ev_conv.png', bbox_inches='tight',dpi=300)
-		
-	
-	
-	wlagdict = {'Age':[],'correlation':[],'Time lag [s]':[],'s':[]}
-	for b in range(nbinseq-1):
-		for s in range(ISC_w_time.shape[1]):
-			xcorrt = xcorr(ev_conv,ISC_w_time[2,s,b])#np.correlate(ev_conv,ISC_w_time[2,s,b],"full")#
-			wlagdict['Age'].extend([xticks[b]]*len(xcorrx))
-			wlagdict['s'].extend([s]*len(xcorrx))
-			wlagdict['correlation'].extend(xcorrt)
-			wlagdict['Time lag [s]'].extend(xcorrx)
-	dfelag = pd.DataFrame(data=wlagdict)
-	sns.set_palette(colors_age)
-	fig,ax = plt.subplots(1,1,figsize=(5,5))
-	g = sns.lineplot(x='Time lag [s]', y='correlation',
-                hue='Age', ax=ax, data=dfelag[abs(dfelag['Time lag [s]'])<20], ci='sd')
-	ax.legend(loc='center', bbox_to_anchor=(0.5, -0.3))
-	plt.savefig(figurepath+'HPC/ISC_xcorr_within_ev_conv.png', bbox_inches='tight')
-
-	import itertools
-	glagdict = {'Age Pair':[],'correlation':[],'Time lag [s]':[],'s':[]}
-	for p in itertools.combinations(range(nbinseq),2):
-		if 4 in p:
-			for s in range(len(ISC_g_time['splithalf'].keys())):
-				ISC = ISC_g_time['splithalf'][s][str(p[0])+'_'+str(p[1])]
-				xcorrt = xcorr(ev_conv,ISC)#np.correlate(ISC,ISC,'full')#
-				glagdict['Age Pair'].extend([xticks[p[0]]+' with '+xticks[p[1]]]*len(xcorrx))
-				glagdict['s'].extend([s]*len(xcorrx))
-				glagdict['correlation'].extend(xcorrt)
-				glagdict['Time lag [s]'].extend(xcorrx)
-	dfglag = pd.DataFrame(data=glagdict)
-	fig,ax = plt.subplots(1,1,figsize=(5,5))
-	g = sns.lineplot(x='Time lag [s]', y='correlation',
-                hue='Age Pair', ax=ax, data=dfglag[abs(dfglag['Time lag [s]'])<20], ci='sd')
-	ax.legend(loc='center', bbox_to_anchor=(0.5, -0.3))
-	plt.savefig(figurepath+'HPC/ISC_xcorr_g_ev_conv.png', bbox_inches='tight')
-
-
-	
-
