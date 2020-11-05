3ed27c92f95992c4dbe291cad0054733a4df825e
Samantha Cohen <samantha.s.cohen@gmail.com>
2020-11-05 12:41:51 -0500

    incorporated 5_parcellation into 6_HMM_n_k, also took out plotting stuff and put into 6_HMM_ll.

--- 6_HMM_n_k.py
+++ 6_HMM_n_k.py
@@ -11,9 +11,9 @@ import brainiak.eventseg.event
 from sklearn.model_selection import KFold
 from HMM_settings import *
 
-sametimedir = HMMpath+'shuff_5bins_train04/'
+roidir = ISCpath+'Yeo_parcellation/'
 nkdir = HMMpath+'nk_moreshuff/'#'nk/'
-nsub= 41
+nsub= 40
 y = [0]*int(np.floor(nsub/nsplit))*4+[1]*(int(np.floor(nsub/nsplit))+1)
 kf = KFold(n_splits=nsplit, shuffle=True, random_state=2)
 bins = [0,4]
@@ -21,7 +21,7 @@ nbins = len(bins)
 nshuff=100
 task = 'DM'
 
-for roi in tqdm.tqdm(glob.glob(sametimedir+'*h5')):
+for roi in tqdm.tqdm(glob.glob(roidir+'*h5')):
 	roi_short = roi.split('/')[-1][:-3]
 	roidict = {str(b):{} for b in bins}
 	taskv = dd.io.load(roi,'/'+task)
@@ -62,79 +62,5 @@ for roi in tqdm.tqdm(glob.glob(nkdir+'*.h5')):
 	roidict[roi_short]['k_diff_p'] = np.sum(abs(data['k_diff'][0])<abs(data['k_diff'][1:]))/nshuff
 	roidict[roi_short]['sig'] = 1 if roidict[roi_short]['k_diff_p']<0.05 else 0
 		
-dd.io.save(HMMpath+'nk2.h5',roidict)
-roidict=dd.io.load(HMMpath+'nk2.h5')
-lldict = dd.io.load(HMMpath+'ll_diff.h5','/seperate')
-import pandas as pd
-df = pd.DataFrame(roidict).T.merge(pd.DataFrame(lldict).T, left_index=True, right_index=True, how='inner')
-thresh =0.002
-df=df[((df['0_2k_diff']>thresh) | (df['4_2k_diff']>thresh))]
-
-import matplotlib.pyplot as plt
-from scipy.stats import pearsonr
-import seaborn as sns
-grey=211/256
-xticks = [str(int(round(eqbins[b])))+' - '+str(int(round(eqbins[b+1])))+' y.o.' for b in bins]
-sns.set(font_scale = 2,rc={'axes.facecolor':(grey,grey,grey)})
-fig,ax=plt.subplots(figsize=(7,5))
-sns.regplot(x='0',y='4',data=df,color='#8856a7',scatter_kws={'s':50})
-ax.grid(False)
-ax.set_xlabel('Number of events in\nYoungest ('+xticks[0]+')')
-ax.set_ylabel('Number of events in\nOldest ('+xticks[1]+')')
-ax.set(xlim=(6, 19),ylim=(6, 27.5))
-#plt.rcParams['axes.xmargin'] = 1
-print('r = '+str(np.round(r,2))+', p = '+str(np.round(p,8)))
-	
-fig.savefig(figurepath+'n_k/'+'k_lim.png',bbox_inches='tight', dpi=300)
-
-
-
-
-'''
-
-color = ['#9ebcda','#8856a7']#['#810f7c','#8856a7']
-
-plt.rcParams.update({'font.size': 30})
-
-lab=''
-
-x = df['0']; y = df['4']
-x1 = df['0'][df['sig']==0]; y1 = df['4'][df['sig']==0]
-x2 = df['0'][df['sig']==1]; y2 = df['4'][df['sig']==1]
-b,m = np.polyfit(x1,y1,1)
-r,p=pearsonr(x1,y1)
-fig, ax = plt.subplots(figsize=(15,15))
-#ax.set_title('r:'+str(np.round(r,2))+', p:'+str(np.round(p,2)))
-minval = np.min(x1)
-maxval = np.max(x1)
-plt.plot(np.unique(x1), np.poly1d(np.polyfit(x1, y1, 1))(np.unique(x1)),color=color[1], linewidth=5)
-ax.set_xlabel('Number of events for\n'+xticks[0]+'\'s', fontsize=35)
-ax.set_ylabel('Number of events for\n'+xticks[1]+'\'s', fontsize=35)
-ax.scatter(x1,y1,color=color[1],s=250,alpha=0.75,label='No Signicant Difference')
-ax.scatter(x2,y2,color=color[0],s=250,label=' Signicant Difference')
-ax.legend(loc="upper left")
-#ax.set_xlim([2, 20])
-#ax.set_ylim([2, 27])
-if lab == '': ax.set_facecolor((grey,grey,grey))
-if lab == '_lab':
-	dftemp = df[df['sig']==1]
-	xtemp=dftemp['0']; ytemp=dftemp['4']
-	for i, txt in enumerate(list(dftemp.index)):
-		if xtemp[i]>ytemp[i]:
-			ax.annotate('\n'.join(txt.split('_')[:-1]), (xtemp[i], ytemp[i]),
-					horizontalalignment="left",
-					verticalalignment="top",color='k',fontsize=10)
-		if xtemp[i]<ytemp[i]:
-			ax.annotate('\n'.join(txt.split('_')[:-1]), (xtemp[i], ytemp[i]),
-					horizontalalignment="right",
-					verticalalignment="bottom",color='k',fontsize=10)
-'''
-		
-
-		
-
-		
-	
-		
-					
+dd.io.save(HMMpath+'nk.h5',roidict)
 	
