ded6839595d85826491cc3e06bff5b18446699bc
Samantha Cohen <samantha.s.cohen@gmail.com>
2020-11-06 14:16:53 -0500

    Modified 6_ISC_n_k - trying to combine with 7_* version.

--- 6_HMM_n_k.py
+++ 6_HMM_n_k.py
@@ -5,7 +5,7 @@
 # Oldest group (bin 4)
 # Is the assumption true that the number of events is equal across groups?
 
-
+import os
 import tqdm
 import brainiak.eventseg.event
 from sklearn.model_selection import KFold
@@ -19,37 +19,50 @@ kf = KFold(n_splits=nsplit, shuffle=True, random_state=2)
 bins = [0,4]
 nbins = len(bins)
 nshuff=100
+nshuff2perm=1000
 task = 'DM'
+
+if os.path.exists(HMMpath+'nk.h5'):
+	pdict=dd.io.load(HMMpath+'nk.h5')
 
 for roi in tqdm.tqdm(glob.glob(roidir+'*h5')):
 	roi_short = roi.split('/')[-1][:-3]
-	roidict = {str(b):{} for b in bins}
-	taskv = dd.io.load(roi,'/'+task)
-	roidict['vall'] = taskv['vall']
-	for b in bins:
-		roidict[str(b)]['D'] = taskv['bin_'+str(b)]['D']
-	Dall = [roidict['0']['D'],roidict['4']['D']]
-	for bi,b in enumerate(bins):
-		notbi = 1 if bi==0 else 0
-		best_k = np.zeros(nshuff+1)
-		tune_ll = np.zeros((nshuff+1,nsplit,len(k_list)))
-		for split,Ls in enumerate(kf.split(np.arange(nsub),y)):
-			Dtrain = np.mean(Dall[bi][Ls[0]],axis=0).T
-			Dtest_all  = np.concatenate((Dall[bi][Ls[1]],Dall[notbi][Ls[1]]),axis=0)
-			subl = np.concatenate((np.ones(len(Ls[1])),np.zeros(len(Ls[1]))),axis=0)
-			for ki,k in enumerate(k_list):
-				hmm = brainiak.eventseg.event.EventSegment(n_events=k)
-				hmm.fit(Dtrain)
-				for shuff in range(nshuff+1):
-					_, tune_ll[shuff,split,ki] = hmm.find_events(np.mean(Dtest_all[subl==1], axis=0).T)
-					# RANDOMIZE
-					subl = np.random.permutation(subl)
+	if (os.path.exists(HMMpath+'nk.h5') and pdict[roi_short]['k_diff_p'] <= 0.05) or \
+	not os.path.exists(HMMpath+'nk.h5'):
+		if os.path.exists(HMMpath+'nk.h5'):
+			roidict = dd.io.load(nkdir+roi+'.h5')
+			nshuff_ = len(roidict['0']['best_k'])-1
+			Dall = [roidict['0']['D'],roidict['4']['D']]
+			nshuff2 = nshuff2perm + nshuff_
+			shuffl = np.arange(nshuff_+1,nshuff2+1)
+		else:
+			roidict = {str(b):{} for b in bins}
+			Ddict = dd.io.load(roi)
+			for b in bins:
+				roidict[str(b)]['D'] = Ddict[task]['bin_'+str(b)]['D']
+			Dall = [roidict['0']['D'],roidict['4']['D']]
+			shuffl = np.arange(nshuff+1)
+		for bi,b in enumerate(bins):
+			notbi = 1 if bi==0 else 0
+			best_k = np.zeros(nshuff+1)
+			tune_ll = np.zeros((nshuff+1,nsplit,len(k_list)))
+			for split,Ls in enumerate(kf.split(np.arange(nsub),y)):
+				Dtrain = np.mean(Dall[bi][Ls[0]],axis=0).T
+				Dtest_all  = np.concatenate((Dall[bi][Ls[1]],Dall[notbi][Ls[1]]),axis=0)
+				subl = np.concatenate((np.ones(len(Ls[1])),np.zeros(len(Ls[1]))),axis=0)
+				for ki,k in enumerate(k_list):
+					hmm = brainiak.eventseg.event.EventSegment(n_events=k)
+					hmm.fit(Dtrain)
+					for shuff in range(nshuff+1):
+						if shuff != 0: # RANDOMIZE
+							subl = np.random.permutation(subl)
+						_, tune_ll[shuff,split,ki] = hmm.find_events(np.mean(Dtest_all[subl==1], axis=0).T)			
 		for shuff in range(nshuff+1):
 			 best_k[shuff] = np.mean([k_list[np.argmax(tune_ll[shuff,ki])] for ki in range(kf.n_splits)])
 		roidict[str(b)]['tune_ll'] = tune_ll
 		roidict[str(b)]['best_k'] = best_k
-	roidict['k_diff'] = roidict[str(4)]['best_k'] - roidict[str(0)]['best_k']
-	dd.io.save(nkdir+roi_short+'.h5',roidict)
+		roidict['k_diff'] = roidict[str(4)]['best_k'] - roidict[str(0)]['best_k']
+		dd.io.save(nkdir+roi_short+'.h5',roidict)
 	
 
 roidict = {}
