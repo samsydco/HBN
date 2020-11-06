8e6850a0a71c1bfc3475cb36c9a26115406fa105
Samantha Cohen <samantha.s.cohen@gmail.com>
2020-11-06 13:34:42 -0500

    Finished combining 6_ISC_sh_yeo and 7_ISC_sh_yeo for additional shuffles.

--- 6_ISC_sh_yeo.py
+++ 6_ISC_sh_yeo.py
@@ -14,7 +14,7 @@ nbins = len(bins)
 roidir = ISCpath+'Yeo_parcellation/'
 savedir = ISCpath+'shuff_Yeo/'
 nsub = 40
-nshuff2=1000
+nshuff2perm=1000
 
 def p_calc(ISC,ISCtype='e'):
 	nshuff = ISC.shape[0]-1
@@ -73,78 +73,39 @@ for roi in tqdm.tqdm(glob.glob(roidir+'*.h5')):
 		shuffl = 0
 		if os.path.exists(savedir+roi_short+'.h5'):
 			taskv = roidict[task]
-			e_p,nshuff_e = p_calc(taskv['ISC_e'],'e')
-			g_p,nshuff_g = p_calc(taskv['ISC_g'],'g')
+			e_p,nshuff_ = p_calc(taskv['ISC_e'],'e')
+			g_p,nshuff_ = p_calc(taskv['ISC_g'],'g')
+			nshuff2 = nshuff2perm + nshuff_
 			if e_p < 0.05 or g_p < 0.05:
-				roidict[task]['ISC_e'] = np.append(taskv['ISC_e'], np.zeros((nshuff2-nshuff_e,n_vox)), axis=0)
-				roidict[task]['ISC_g'] = np.append(taskv['ISC_g'], np.zeros((nshuff2-nshuff_g,n_vox)), axis=0)
-				roidict[task]['ISC_g_time'] = np.append(taskv['ISC_g_time'], np.zeros((nshuff2-nshuff_g,n_vox,n_time)), axis=0)
-				shuffl = np.arange(nshuff_e+1,nshuff2+1)
+				roidict[task]['ISC_w'] = np.append(taskv['ISC_w'], np.zeros((nshuff2-nshuff_,nbins,n_vox)), axis=0)
+				roidict[task]['ISC_e'] = np.append(taskv['ISC_e'], np.zeros((nshuff2-nshuff_,n_vox)), axis=0)
+				roidict[task]['ISC_b'] = np.append(taskv['ISC_b'], np.zeros((nshuff2-nshuff_,4,n_vox)), axis=0)
+				roidict[task]['ISC_g'] = np.append(taskv['ISC_g'], np.zeros((nshuff2-nshuff_,n_vox)), axis=0)
+				roidict[task]['ISC_g_time'] = np.append(taskv['ISC_g_time'], np.zeros((nshuff2-nshuff_,n_vox,n_time)), axis=0)
+				shuffl = np.arange(nshuff_+1,nshuff2+1)
 		else:
-			roidict[task] = {'vall':vall, 'ISC_w':np.zeros((nshuff+1,nbins,n_vox)), 'ISC_w_time':np.zeros((nshuff+1,nbins,n_vox,n_time)), 'ISC_g_time':np.zeros((nshuff+1,n_vox,n_time)), 'ISC_g':np.zeros((nshuff+1,n_vox)), 'ISC_e':np.zeros((nshuff+1,n_vox))}
+			roidict[task] = {'vall':vall, 'ISC_w':np.zeros((nshuff+1,nbins,n_vox)), 'ISC_b':np.zeros((nshuff+1,4,n_vox)), 'ISC_g_time':np.zeros((nshuff+1,n_vox,n_time)), 'ISC_g':np.zeros((nshuff+1,n_vox)), 'ISC_e':np.zeros((nshuff+1,n_vox))}
 			shuffl = np.arange(nshuff+1)
 		for shuff in shuffl:
 			if shuff !=0:
 				Age,Sex = shuff_demo(Age,Sex)
 			subh = even_out(Age,Sex)
-			ISC_w,groups = ISC_w_calc(D,n_vox,n_time,nsub,subh)
-			roidict[task]['ISC_e'][shuff] = ISC_w[0] - ISC_w[1]
-			
-			groups = np.zeros((nbins,2,n_vox,n_time),dtype='float16')
-			for h in [0,1]:
-				for htmp in [0,1]:
-					group = np.zeros((n_vox,n_time),dtype='float16')
-					groupn = np.ones((n_vox,n_time),dtype='int')*nsub//2
-					for i in subh[h][htmp]:
-						group = np.nansum(np.stack((group,D[i])),axis=0)
-						nanverts = np.argwhere(np.isnan(D[i,:]))
-						groupn[nanverts[:, 0],nanverts[:,1]] = groupn[nanverts[:,0],nanverts[:,1]]-1
-					groups[h,htmp] = zscore(group/groupn,axis=1)
-				roidict[task]['ISC_w_time'][shuff,h] = np.multiply(groups[h,0],groups[h,1])
-				roidict[task]['ISC_w'][shuff,h] = np.sum(roidict[task]['ISC_w_time'][shuff,h],axis=1)/(n_time-1)
+			roidict[task]['ISC_w'][shuff], groups =\
+				ISC_w_calc(D,n_vox,n_time,nsub,subh)
+			roidict[task]['ISC_e'][shuff] = roidict[task]['ISC_w'][shuff,0] -\
+											roidict[task]['ISC_w'][shuff,1]
 			ISC_b_time = []
+			idx = 0
 			for htmp1 in [0,1]:
 				for htmp2 in [0,1]:
 					ISC_b_time.append(np.multiply(groups[0,htmp1], groups[1,htmp2]))
-			# Now calculate g_diff and e_diff
-			ISCg_time = np.sum(ISC_b_time,axis=0)
+					roidict[task]['ISC_b'][shuff,idx] = [np.sum(ISC_b_time[-1], axis=1)/(n_time-1)
+					idx+=1
 			denom = np.sqrt(roidict[task]['ISC_w'][shuff,0]) * \
 					np.sqrt(roidict[task]['ISC_w'][shuff,1])
 			roidict[task]['ISC_g_time'][shuff] = np.sum(ISC_b_time, axis=0)/4/np.tile(denom,(n_time,1)).T
-			roidict[task]['ISC_g'][shuff] = np.sum([np.sum(i,axis=1)/(n_time-1) for i in ISC_b_time], axis=0)/4/denom
-			roidict[task]['ISC_e'][shuff] = roidict[task]['ISC_w'][shuff,0] - \
-					roidict[task]['ISC_w'][shuff,1]
+			roidict[task]['ISC_g'][shuff] = np.sum(roidict[task]['ISC_b'][shuff], axis=0)/4/denom
 	dd.io.save(savedir+roi_short+'.h5',roidict)
-	
-	
-# Do any ROIs significantly correlate with ev_conv?
-from HMM_settings import *
-sigroig = {}
-sigroie = {}
-for roi in tqdm.tqdm(glob.glob(savedir+'*.h5')):
-	roi_short = roi.split('/')[-1][:-3]
-	ISC_g_time = dd.io.load(roi,'/DM/ISC_g_time')
-	ISC_e_time = dd.io.load(roi,'/DM/ISC_w_time')[:,1]-dd.io.load(roi,'/DM/ISC_w_time')[:,0]
-	nshuff = ISC_g_time.shape[0]
-	rlg = np.zeros(nshuff)
-	rle = np.zeros(nshuff)
-	for shuff in range(nshuff):
-		rlg[shuff],p = pearsonr(np.nanmean(ISC_g_time[shuff],axis=0),ev_conv)
-		rle[shuff],p = pearsonr(np.nanmean(ISC_e_time[shuff],axis=0),ev_conv)
-	pvalg = np.sum(rlg[0]>rlg[1:])/(nshuff-1)
-	pvale = np.sum(rle[0]>rle[1:])/(nshuff-1)
-	if pvalg < 0.05:
-		sigroig[roi_short] = {'r':rlg[0],'p':pvalg}
-	if pvale < 0.05:
-		sigroie[roi_short] = {'r':rle[0],'p':pvale}
-		
 
-				
-				
-			
-				
-			
-			
-			
-			
-			
+														 
+														 
