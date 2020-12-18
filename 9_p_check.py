5c70297724a1291012afb444b1bce9a1a727421d
Samantha Cohen <samantha.s.cohen@gmail.com>
2020-12-18 16:31:12 -0500

    changes to 9_p_check.py.

--- 9_p_check.py
+++ 9_p_check.py
@@ -18,11 +18,11 @@ for roi in tqdm.tqdm(glob.glob(roidir+'*h5')):
 	roi_short = roi.split('/')[-1][:-3]
 	savedict[roi_short] = {}
 	savedict[roi_short]['vall'] = dd.io.load(roi,'/vall')
-	ISCvals = dd.io.load(ISCdir+roi_short+'.h5','/'+task)
 	for isc in ['ISC_e','ISC_g']:
+		ISCvals = dd.io.load(ISCdir+roi_short+'.h5','/'+task+'/'+isc)
 		savedict[roi_short][isc] = {}
-		savedict[roi_short][isc]['val'] = np.nanmean(ISCvals[isc][0])
-		savedict[roi_short][isc]['shuff'] = np.nanmean(ISCvals[isc][1:],axis=1)
+		savedict[roi_short][isc]['val'] = np.nanmean(ISCvals[0])
+		savedict[roi_short][isc]['shuff'] = np.nanmean(ISCvals[1:],axis=1)
 		if 'g' in isc:
 			savedict[roi_short][isc]['p'] = np.sum(abs(savedict[roi_short][isc]['val']) > savedict[roi_short][isc]['shuff'])/len(savedict[roi_short][isc]['shuff'])
 		else:
@@ -34,12 +34,12 @@ for roi in tqdm.tqdm(glob.glob(roidir+'*h5')):
 		savedict[roi_short]['k_diff']['shuff'] = df.loc[roi_short]['shuff']
 		savedict[roi_short]['k_diff']['p'] = df.loc[roi_short]['k_diff_p']
 		savedict[roi_short]['k_diff']['q'] = df.loc[roi_short]['k_diff_q']
-		
-		HMMvals = dd.io.load(HMMdir+roi_short+'.h5','/'+task)
+	
 		for HMMd in ['ll_diff','auc_diff']:
+			HMMvals = dd.io.load(HMMdir+roi_short+'.h5','/'+task+'/'+HMMd)
 			savedict[roi_short][HMMd] = {}
-			savedict[roi_short][HMMd]['val'] = HMMvals[HMMd][0]
-			savedict[roi_short][HMMd]['shuff'] = HMMvals[HMMd][1:]
+			savedict[roi_short][HMMd]['val'] = HMMvals[0]
+			savedict[roi_short][HMMd]['shuff'] = HMMvals[1:]
 			savedict[roi_short][HMMd]['p'] = np.sum(abs(savedict[roi_short][HMMd]['val'])<\
 				abs(savedict[roi_short][HMMd]['shuff']))/len(savedict[roi_short][HMMd]['shuff'])
 			
@@ -50,7 +50,10 @@ for comp in ['ISC_e','ISC_g','ll_diff','auc_diff']:
 	for roi in savedict.keys():
 		if comp in savedict[roi].keys():
 			ROIl.append(roi)
-			ps.append(savedict[roi][comp]['p'])
+			if savedict[roi][comp]['p'] == 0:
+				ps.append(1/(len(savedict[roi][comp]['shuff'])+1))
+			else:
+				ps.append(savedict[roi][comp]['p'])
 	qs = FDR_p(np.array(ps))
 	for i,roi in enumerate(ROIl):
 		savedict[roi][comp]['q'] = qs[i]
